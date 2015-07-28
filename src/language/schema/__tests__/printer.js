23046af3b7e21c608e26f632c6de08fec4291198
Lee Byron <lee@leebyron.com>
2015-07-28 00:26:48 -0700

    Add visitor and printer utils to schema language
    This adds a "visitor" and "printer" utilities for the schema language in the same form as those used for the document language.

---
+++ src/language/schema/__tests__/printer.js
@@ -0,0 +1,82 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { parseSchemaIntoAST } from '../parser';
+import { printSchema } from '../printer';
+
+describe('Printer', () => {
+
+  it('prints minimal ast', () => {
+    var ast = {
+      kind: 'ScalarDefinition',
+      name: { kind: 'Name', value: 'foo' }
+    };
+    expect(printSchema(ast)).to.equal('scalar foo');
+  });
+
+  it('produces helpful error messages', () => {
+    var badAst1 = { random: 'Data' };
+    expect(() => printSchema(badAst1)).to.throw(
+      'Invalid AST Node: {"random":"Data"}'
+    );
+  });
+
+  var kitchenSink = readFileSync(
+    join(__dirname, '/schema-kitchen-sink.graphql'),
+    { encoding: 'utf8' }
+  );
+
+  it('does not alter ast', () => {
+    var ast = parseSchemaIntoAST(kitchenSink);
+    var astCopy = JSON.parse(JSON.stringify(ast));
+    printSchema(ast);
+    expect(ast).to.deep.equal(astCopy);
+  });
+
+  it('prints kitchen sink', () => {
+
+    var ast = parseSchemaIntoAST(kitchenSink);
+
+    var printed = printSchema(ast);
+
+    expect(printed).to.equal(
+`type Foo implements Bar {
+  one: Type
+  two(argument: InputType!): Type
+  three(argument: InputType, other: String): Int
+  four(argument: String = "string"): String
+  five(argument: [String] = ["string", "string"]): String
+  six(argument: InputType = {key: "value"}): Type
+}
+
+interface Bar {
+  one: Type
+  four(argument: String = "string"): String
+}
+
+union Feed = Story | Article | Advert
+
+scalar CustomScalar
+
+enum Site {
+  DESKTOP
+  MOBILE
+}
+
+input InputType {
+  key: String!
+}
+`);
+
+  });
+});
---
+++ src/language/schema/visitor.js
@@ -0,0 +1,44 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+import { visit, QueryDocumentKeys } from '../visitor';
+
+
+export var SchemaKeys = {
+  Name: QueryDocumentKeys.Name,
+
+  SchemaDocument: ['definitions'],
+  TypeDefinition: ['name', 'interfaces', 'fields'],
+  FieldDefinition: ['name', 'arguments', 'type'],
+  ArgumentDefinition: ['name', 'type', 'defaultValue'],
+  InterfaceDefinition: ['name', 'fields'],
+  UnionDefinition: ['name', 'types'],
+  ScalarDefinition: ['name'],
+  EnumDefinition: ['name', 'values'],
+  EnumValueDefinition: ['name'],
+  InputObjectDefinition: ['name', 'fields'],
+  InputFieldDefinition: ['name', 'type'],
+
+  IntValue: QueryDocumentKeys.IntValue,
+  FloatValue: QueryDocumentKeys.FloatValue,
+  StringValue: QueryDocumentKeys.StringValue,
+  BooleanValue: QueryDocumentKeys.BooleanValue,
+  EnumValue: QueryDocumentKeys.EnumValue,
+  ListValue: QueryDocumentKeys.ListValue,
+  ObjectValue: QueryDocumentKeys.ObjectValue,
+  ObjectField: QueryDocumentKeys.ObjectField,
+
+  NamedType: QueryDocumentKeys.NamedType,
+  ListType: QueryDocumentKeys.ListType,
+  NonNullType: QueryDocumentKeys.NonNullType,
+};
+
+export function visitSchema(root, visitor, keys) {
+  return visit(root, visitor, keys || SchemaKeys);
+}
