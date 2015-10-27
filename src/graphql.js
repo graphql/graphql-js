4355c40bc0d5873fdfb18e1ef398ba298232b031
Lee Byron <lee@leebyron.com>
2015-10-26 18:45:39 -0700

    Documentation for main graphql() function
    Improves #194

--- src/graphql.js
+++ src/graphql.js
@@ -24,6 +24,21 @@ import type { GraphQLSchema } from './type/schema';
  * More sophisticated GraphQL servers, such as those which persist queries,
  * may wish to separate the validation and execution phases to a static time
  * tooling step, and a server runtime step.
+ *
+ * schema:
+ *    The GraphQL type system to use when validating and executing a query.
+ * requestString:
+ *    A GraphQL language formatted string representing the requested operation.
+ * rootValue:
+ *    The value provided as the first argument to resolver functions on the top
+ *    level type (e.g. the query object type).
+ * variableValues:
+ *    A mapping of variable name to runtime value to use for all variables
+ *    defined in the requestString.
+ * operationName:
+ *    The name of the operation to use if requestString contains multiple
+ *    possible operations. Can be omitted if requestString contains only
+ *    one operation.
  */
 export function graphql(
   schema: GraphQLSchema,
