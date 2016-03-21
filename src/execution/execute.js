e70934f8e37ce75e25414f34ed6a32b1bff8b74a
Oscar Lafarga <otech47@gmail.com>
2016-03-21 13:57:16 -0400

    Issue #54 fix: only 1 case tested adding an operation name of 'mutation' for a mutation query in a post request

--- src/execution/execute.js
+++ src/execution/execute.js
@@ -174,7 +174,7 @@ function buildExecutionContext(
             'Must provide operation name if query contains multiple operations.'
           );
         }
-        if (!operationName ||
+        if (operationName ||
             definition.name && definition.name.value === operationName) {
           operation = definition;
         }
