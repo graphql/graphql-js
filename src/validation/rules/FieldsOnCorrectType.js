e28ffb9da40e54f222013aa0adadd7de1d2b3cf6
Joe McBride <joe@uicraftsman.com>
2016-08-25 22:41:56 -0700

    [minor] Fix typo in validation rule comments

--- src/validation/rules/FieldsOnCorrectType.js
+++ src/validation/rules/FieldsOnCorrectType.js
@@ -42,7 +42,7 @@ export function undefinedFieldMessage(
  * Fields on correct type
  *
  * A GraphQL document is only valid if all fields selected are defined by the
- * parent type, or are an allowed meta field such as __typenamme
+ * parent type, or are an allowed meta field such as __typename.
  */
 export function FieldsOnCorrectType(context: ValidationContext): any {
   return {
