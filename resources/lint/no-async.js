31cb269e763fb8268b7238f6833a828d60df4deb
Greg Hurrell <greg@hurrell.net>
2017-08-25 21:24:33 -0700

    Add linter error preventing use of `async` outside the test suite
    We want to keep the runtime dependency footprint small, so that means
    avoiding use of `async` at runtime (which creates a dependency on the
    Regenerator runtime).
    
    There is no built-in eslint rule for this, so we make a custom one.
    
    Test plan:
    
    Add an `async` function to a file, run `yarn run lint` and see:
    
        graphql-js/src/subscription/subscribe.js
        288:1  error  async functions are not allowed outside of the test suite  no-async
    
        ✖ 1 problem (1 error, 0 warnings)
    
    Note that no errors are issued for the `async` functions in the test
    suite.
    
    Closes: https://github.com/graphql/graphql-js/issues/1008

---
+++ resources/lint/no-async.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2017, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+module.exports = function(context) {
+  if (context.getFilename().match(/\b__tests__\b/)) {
+    return {};
+  } else {
+    return {
+      FunctionDeclaration: function(node) {
+        if (node.async) {
+          context.report(
+            node,
+            'async functions are not allowed outside of the test suite'
+          );
+        }
+      },
+    };
+  }
+};
