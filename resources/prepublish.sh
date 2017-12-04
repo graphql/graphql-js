8f2db6ac8952a27520ccd95be7969efcef6e30eb
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-12-04 20:06:24 +0200

    Fix `yarn` cmd. check the length of npm_config_argv in prepublish (#1110)

--- resources/prepublish.sh
+++ resources/prepublish.sh
@@ -1,7 +1,7 @@
 # Because of a long-running npm issue (https://github.com/npm/npm/issues/3059)
 # prepublish runs after `npm install` and `npm pack`.
 # In order to only run prepublish before `npm publish`, we have to check argv.
-if node -e "process.exit(($npm_config_argv).original[0].indexOf('pu') === 0)"; then
+if node -e "process.exit(($npm_config_argv).original.length > 0 && ($npm_config_argv).original[0].indexOf('pu') === 0)"; then
   exit 0;
 fi
 
