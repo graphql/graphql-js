0091397aa8ef1dbd092e47a1f932780fe389faea
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2017-12-29 17:23:02 +0200

    Add 'introspectionFromSchema' utility function
    Based on #1114 by @mjmahone

---
+++ src/utilities/__tests__/introspectionFromSchema-test.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import dedent from '../../jsutils/dedent';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from '../../type';
+import { printSchema } from '../schemaPrinter';
+import { buildClientSchema } from '../buildClientSchema';
+import { introspectionFromSchema } from '../introspectionFromSchema';
+
+function introspectionToSDL(introspection) {
+  return printSchema(buildClientSchema(introspection));
+}
+
+describe('introspectionFromSchema', () => {
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'Simple',
+      description: 'This is a simple type',
+      fields: {
+        string: {
+          type: GraphQLString,
+          description: 'This is a string field',
+        },
+      },
+    }),
+  });
+
+  it('converts a simple schema', () => {
+    const introspection = introspectionFromSchema(schema);
+
+    expect(introspectionToSDL(introspection)).to.deep.equal(dedent`
+      schema {
+        query: Simple
+      }
+
+      """This is a simple type"""
+      type Simple {
+        """This is a string field"""
+        string: String
+      }
+    `);
+  });
+
+  it('converts a simple schema without descriptions', () => {
+    const introspection = introspectionFromSchema(schema, {
+      descriptions: false,
+    });
+
+    expect(introspectionToSDL(introspection)).to.deep.equal(dedent`
+      schema {
+        query: Simple
+      }
+
+      type Simple {
+        string: String
+      }
+    `);
+  });
+});
