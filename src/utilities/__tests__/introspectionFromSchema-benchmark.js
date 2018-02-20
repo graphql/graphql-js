5ec393efc6918cad906b93f3f71d842547b70563
mohawk2 <mohawk2@users.noreply.github.com>
2018-02-20 21:18:48 +0000

    Benchmark introspecting a schema built from SDL in a supplied file. (#1163)
    * Benchmark introspecting a schema built from SDL in a supplied file.
    
    Use it like so:
    
    ```shell
    yarn run benchmark
    ````
    
    Additionally there is a profile command which runs the benchmark with `--prof`:
    
    ```shell
    yarn run profile
    ````
    
    Note that on Ubuntu 16.04 this requires:
    
    ```shell
    sudo apt install linux-tools-common gawk linux-tools-generic linux-tools-4.10.0-35-generic
    sudo sh -c 'echo 1 >/proc/sys/kernel/perf_event_paranoid'
    ```
    
    On my laptop it produces this:
    
    ```
    $ yarn run benchmark
    
    > graphql@0.12.3 benchmark /home/osboxes/graphql-js
    > babel-node ./resources/benchmark/run.js
    
    introspectionQuery x 24.59 ops/sec ±3.87% (45 runs sampled)
    ```
    
    * Move into benchmark tests

---
+++ src/utilities/__tests__/introspectionFromSchema-benchmark.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { execute, parse } from '../../';
+import { buildASTSchema } from '../buildASTSchema';
+import { getIntrospectionQuery } from '../introspectionQuery';
+
+const queryAST = parse(getIntrospectionQuery());
+const schema = buildASTSchema(
+  parse(readFileSync(join(__dirname, 'github-schema.graphql'), 'utf8')),
+);
+
+export const name = 'Execute Introspection Query';
+export function measure() {
+  execute(schema, queryAST);
+}
