3078b1b40bb346da20d6c896c40ec3d41b63acf9
Matt Krick <matt.krick@gmail.com>
2018-03-29 18:55:55 -0700

    lint

--- src/type/__tests__/schema-test.js
+++ src/type/__tests__/schema-test.js
@@ -23,12 +23,6 @@ const InterfaceType = new GraphQLInterfaceType({
   fields: { fieldName: { type: GraphQLString } },
 });
 
-const ImplementingType = new GraphQLObjectType({
-  name: 'Object',
-  interfaces: [InterfaceType],
-  fields: { fieldName: { type: GraphQLString, resolve: () => '' } },
-});
-
 const DirectiveInputType = new GraphQLInputObjectType({
   name: 'DirInput',
   fields: {
