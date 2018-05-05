42323423e8425d9766893c66368b26a3eec6b1b6
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-05-05 23:27:13 +0300

    Fix npm@6 warnings about deprecated "prepublish"

--- resources/prepublish.sh
+++ resources/prepublish.sh
@@ -1,10 +1,3 @@
-# Because of a long-running npm issue (https://github.com/npm/npm/issues/3059)
-# prepublish runs after `npm install` and `npm pack`.
-# In order to only run prepublish before `npm publish`, we have to check argv.
-if node -e "process.exit(($npm_config_argv).original.length > 0 && ($npm_config_argv).original[0].indexOf('pu') === 0)"; then
-  exit 0;
-fi
-
 # Publishing to NPM is currently supported by Travis CI, which ensures that all
 # tests pass first and the deployed module contains the correct file structure.
 # In order to prevent inadvertently circumventing this, we ensure that a CI
