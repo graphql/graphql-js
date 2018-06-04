e3146b5d5eab9c254e691342e2bb4b0d5c0356ec
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-06-04 22:21:28 +0300

    Reject Infinity supplied as Int or Float value

---
+++ src/jsutils/isInteger.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
+declare function isInteger(value: mixed): boolean %checks(typeof value ===
+  'number');
+
+/* eslint-disable no-redeclare */
+// $FlowFixMe workaround for: https://github.com/facebook/flow/issues/4441
+const isInteger =
+  Number.isInteger ||
+  function(value) {
+    return (
+      typeof value === 'number' &&
+      isFinite(value) &&
+      Math.floor(value) === value
+    );
+  };
+export default isInteger;
