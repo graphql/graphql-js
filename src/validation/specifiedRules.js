1e24f8799c294d87546dc75d39ecd91cba4caf8e
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-08-04 00:36:06 +0300

    Add more rules to validate SDL

--- src/validation/specifiedRules.js
+++ src/validation/specifiedRules.js
@@ -129,4 +129,6 @@ export const specifiedSDLRules: $ReadOnlyArray<SDLValidationRule> = [
   LoneSchemaDefinition,
   KnownDirectives,
   UniqueDirectivesPerLocation,
+  UniqueArgumentNames,
+  UniqueInputFieldNames,
 ];
