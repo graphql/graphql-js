bdeaccf46342f5bec36ee2657f14ed2987e02d11
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-08-26 00:45:37 +0300

    Update babel & conver "targets" to browserslist format

--- .babelrc.js
+++ .babelrc.js
@@ -1,16 +1,14 @@
 module.exports = {
   "presets": [["@babel/preset-env", {
     "modules": process.env.BABEL_MODULES ? false : 'commonjs',
-    "targets": {
-      "node": 6,
-      "browsers": [
-        "ie 9",
-        "ios 9",
-        "last 2 chrome versions",
-        "last 2 edge versions",
-        "last 2 firefox versions",
-      ]
-    }
+    "targets": [
+      "node 6",
+      "ie 9",
+      "ios 9",
+      "last 2 chrome versions",
+      "last 2 edge versions",
+      "last 2 firefox versions",
+    ],
   }]],
   "plugins": [
     "./resources/inline-invariant",
