55d9c776f86684526bd59a775f5cdc805da61b79
Travis CI <github@fb.com>
2018-08-30 12:36:53 +0000

    Deploy c2dfccf5a8777a5b6db45eeade63ecf08c86ab13 to NPM branch

--- __fixtures__/index.mjs
+++
@@ -1,9 +0,0 @@
-import { join } from 'path';
-import { readFileSync } from 'fs';
-
-function readLocalFile(filename) {
-  return readFileSync(join(__dirname, filename), 'utf8');
-}
-
-export var bigSchemaSDL = readLocalFile('github-schema.graphql');
-export var bigSchemaIntrospectionResult = JSON.parse(readLocalFile('github-schema.json'));
