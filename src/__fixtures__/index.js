c45add8398d63838dd8cacb68fb94650a8b6e986
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-09-23 22:18:10 +0300

    Explicitly annotate (@flow/@noflow) every JS file

--- src/__fixtures__/index.js
+++ src/__fixtures__/index.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2018-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ */
+
 import { join } from 'path';
 import { readFileSync } from 'fs';
 
