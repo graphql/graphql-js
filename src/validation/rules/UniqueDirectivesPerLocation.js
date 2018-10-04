0dff21fb3508191cc7efbaeff1d2e72135dd9809
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2018-10-04 23:18:41 +0200

    Updated `UniqueDirectivesPerLocation` validation docs

--- src/validation/rules/UniqueDirectivesPerLocation.js
+++ src/validation/rules/UniqueDirectivesPerLocation.js
@@ -27,7 +27,7 @@ export function duplicateDirectiveMessage(directiveName: string): string {
 /**
  * Unique directive names per location
  *
- * A GraphQL document is only valid if all directives at a given location
+ * A GraphQL document is only valid if directives, that are not marked as `isRepeatable`, at a given location
  * are uniquely named.
  */
 export function UniqueDirectivesPerLocation(
