fca5484e7e6bd850635e26bca0559af979e13ce9
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-12-12 18:32:10 +0200

    Rename exported functions to match filenames

--- src/jsutils/defineToStringTag.js
+++ src/jsutils/defineToStringTag.js
@@ -8,19 +8,19 @@
  */
 
 /**
- * The `applyToStringTag()` function checks first to see if the runtime
+ * The `defineToStringTag()` function checks first to see if the runtime
  * supports the `Symbol` class and then if the `Symbol.toStringTag` constant
  * is defined as a `Symbol` instance. If both conditions are met, the
  * Symbol.toStringTag property is defined as a getter that returns the
  * supplied class constructor's name.
  *
- * @method applyToStringTag
+ * @method defineToStringTag
  *
  * @param {Class<any>} classObject a class such as Object, String, Number but
  * typically one of your own creation through the class keyword; `class A {}`,
  * for example.
  */
-export default function applyToStringTag(classObject: Class<any>): void {
+export default function defineToStringTag(classObject: Class<any>): void {
   if (typeof Symbol === 'function' && Symbol.toStringTag) {
     Object.defineProperty(classObject.prototype, Symbol.toStringTag, {
       get() {
