3fbd07622bf42e939c758872050cf6298a64c029
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-12-16 00:58:49 +0200

    Run prettier on all scripts/configs JS code

--- .babelrc.js
+++ .babelrc.js
@@ -1,20 +1,25 @@
 module.exports = {
-  "presets": [["@babel/preset-env", {
-    "modules": process.env.BABEL_MODULES ? false : 'commonjs',
-    "targets": [
-      "node 6",
-      "ie 9",
-      "ios 9",
-      "last 2 chrome versions",
-      "last 2 edge versions",
-      "last 2 firefox versions",
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: process.env.BABEL_MODULES ? false : 'commonjs',
+        targets: [
+          'node 6',
+          'ie 9',
+          'ios 9',
+          'last 2 chrome versions',
+          'last 2 edge versions',
+          'last 2 firefox versions',
+        ],
+      },
     ],
-  }]],
-  "plugins": [
-    "./resources/inline-invariant",
-    "@babel/plugin-transform-flow-strip-types",
-    ["@babel/plugin-transform-classes", {"loose": true}],
-    ["@babel/plugin-transform-destructuring", {"loose": true}],
-    ["@babel/plugin-transform-spread", {"loose": true}]
-  ]
+  ],
+  plugins: [
+    './resources/inline-invariant',
+    '@babel/plugin-transform-flow-strip-types',
+    ['@babel/plugin-transform-classes', { loose: true }],
+    ['@babel/plugin-transform-destructuring', { loose: true }],
+    ['@babel/plugin-transform-spread', { loose: true }],
+  ],
 };
