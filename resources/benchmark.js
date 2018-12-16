2c853a59f68d668a69f5a18b892d8a02c95b4958
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2018-12-16 02:00:18 +0200

    Mark all 'resources/*.js' files as strict and @noflow

--- resources/benchmark.js
+++ resources/benchmark.js
@@ -3,7 +3,11 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @noflow
  */
+
+'use strict';
 
 const { Suite } = require('benchmark');
 const beautifyBenchmark = require('beautify-benchmark');
--- resources/copy-package-json.js
+++ resources/copy-package-json.js
@@ -3,7 +3,11 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @noflow
  */
+
+'use strict';
 
 /**
  * Ensure a vanilla package.json before deploying so other tools do not
@@ -13,17 +17,17 @@
 const fs = require('fs');
 const assert = require('assert');
 
-const package = require('../package.json');
-delete package.scripts;
-delete package.devDependencies;
+const packageJSON = require('../package.json');
+delete packageJSON.scripts;
+delete packageJSON.devDependencies;
 
-const { version } = package;
+const { version } = packageJSON;
 const match = /^[0-9]+\.[0-9]+\.[0-9]+-?([^.]*)/.exec(version);
 assert(match, 'Version does not match semver spec.');
 const tag = match[1];
 assert(!tag || tag === 'rc', 'Only "rc" tag is supported.');
 
-assert(!package.publishConfig, 'Can not override "publishConfig".');
-package.publishConfig = { tag: tag || 'latest' };
+assert(!packageJSON.publishConfig, 'Can not override "publishConfig".');
+packageJSON.publishConfig = { tag: tag || 'latest' };
 
-fs.writeFileSync('./dist/package.json', JSON.stringify(package, null, 2));
+fs.writeFileSync('./dist/package.json', JSON.stringify(packageJSON, null, 2));
