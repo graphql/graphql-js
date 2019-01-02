54db6e4869a22ef65e501a3e7f81c6a766a4a1a5
Ryan Bonte <ryan.bonte@gmail.com>
2019-01-02 22:07:43 +0100

    added sdl tests

--- src/utilities/__tests__/stripIgnoredTokens-test.js
+++ src/utilities/__tests__/stripIgnoredTokens-test.js
@@ -16,7 +16,7 @@ import { join } from 'path';
 import dedent from '../../jsutils/dedent';
 import { stripIgnoredTokens } from '../stripIgnoredTokens';
 
-describe('Strip ignored tokens: Query document', () => {
+describe('stripIgnoredTokens: Query document', () => {
   const kitchenSink = readFileSync(
     join(
       __dirname,
@@ -270,6 +270,219 @@ describe('Strip ignored tokens: Query document', () => {
     const resultStripped = String.raw`query queryName($foo:ComplexType$site:Site=MOBILE)@onQuery{whoever123is:node(id:[123 456]){id...on User@onInlineFragment{field2{id alias:field1(first:10 after:$foo)@include(if:$foo){id...frag@onFragmentSpread}}}...@skip(unless:$foo){id}...{id}}}mutation likeStory@onMutation{like(story:123)@onField{story{id@onField}}}subscription StoryLikeSubscription($input:StoryLikeSubscribeInput)@onSubscription{storyLikeSubscribe(input:$input){story{likers{count}likeSentence{text}}}}fragment frag on Friend@onFragmentDefinition{foo(size:$size bar:$b obj:{key:"value" block:"""
     block string uses \"""
   """})}{unnamed(truthy:true falsey:false nullish:null)query}{__typename}`;
+
+    expect(printedStripped).to.equal(resultStripped);
+
+    expect(() => parse(printedStripped)).to.not.throw();
+
+    const parsedStrippedAst = parse(printedStripped);
+
+    const printedAgain = print(parsedStrippedAst);
+
+    expect(printedAgain).to.equal(result);
+  });
+});
+
+describe('stripIgnoredTokens: SDL document', () => {
+  it('prints minimal ast', () => {
+    const ast = {
+      kind: 'ScalarTypeDefinition',
+      name: { kind: 'Name', value: 'foo' },
+    };
+    expect(stripIgnoredTokens(print(ast))).to.equal('scalar foo');
+  });
+
+  it('produces helpful error messages', () => {
+    const badAst1 = { random: 'Data' };
+    expect(() => stripIgnoredTokens(print(badAst1))).to.throw(
+      'Invalid AST Node: { random: "Data" }',
+    );
+  });
+
+  const kitchenSink = readFileSync(
+    join(
+      __dirname,
+      '../',
+      '../',
+      '/language',
+      '/__tests__',
+      '/schema-kitchen-sink.graphql',
+    ),
+    { encoding: 'utf8' },
+  );
+
+  it('does not alter ast', () => {
+    const ast = parse(kitchenSink);
+    const astBefore = JSON.stringify(ast);
+    stripIgnoredTokens(print(ast));
+    expect(JSON.stringify(ast)).to.equal(astBefore);
+  });
+
+  it('prints kitchen sink', () => {
+    const ast = parse(kitchenSink);
+
+    const printed = print(ast);
+
+    const result = dedent`
+      schema {
+        query: QueryType
+        mutation: MutationType
+      }
+
+      """
+      This is a description
+      of the \`Foo\` type.
+      """
+      type Foo implements Bar & Baz {
+        "Description of the \`one\` field."
+        one: Type
+        """
+        This is a description of the \`two\` field.
+        """
+        two(
+          """
+          This is a description of the \`argument\` argument.
+          """
+          argument: InputType!
+        ): Type
+        """
+        This is a description of the \`three\` field.
+        """
+        three(argument: InputType, other: String): Int
+        four(argument: String = "string"): String
+        five(argument: [String] = ["string", "string"]): String
+        six(argument: InputType = {key: "value"}): Type
+        seven(argument: Int = null): Type
+      }
+
+      type AnnotatedObject @onObject(arg: "value") {
+        annotatedField(arg: Type = "default" @onArgumentDefinition): Type @onField
+      }
+
+      type UndefinedType
+
+      extend type Foo {
+        seven(argument: [String]): Type
+      }
+
+      extend type Foo @onType
+
+      interface Bar {
+        one: Type
+        four(argument: String = "string"): String
+      }
+
+      interface AnnotatedInterface @onInterface {
+        annotatedField(arg: Type @onArgumentDefinition): Type @onField
+      }
+
+      interface UndefinedInterface
+
+      extend interface Bar {
+        two(argument: InputType!): Type
+      }
+
+      extend interface Bar @onInterface
+
+      union Feed = Story | Article | Advert
+
+      union AnnotatedUnion @onUnion = A | B
+
+      union AnnotatedUnionTwo @onUnion = A | B
+
+      union UndefinedUnion
+
+      extend union Feed = Photo | Video
+
+      extend union Feed @onUnion
+
+      scalar CustomScalar
+
+      scalar AnnotatedScalar @onScalar
+
+      extend scalar CustomScalar @onScalar
+
+      enum Site {
+        """
+        This is a description of the \`DESKTOP\` value
+        """
+        DESKTOP
+        """
+        This is a description of the \`MOBILE\` value
+        """
+        MOBILE
+        "This is a description of the \`WEB\` value"
+        WEB
+      }
+
+      enum AnnotatedEnum @onEnum {
+        ANNOTATED_VALUE @onEnumValue
+        OTHER_VALUE
+      }
+
+      enum UndefinedEnum
+
+      extend enum Site {
+        VR
+      }
+
+      extend enum Site @onEnum
+
+      input InputType {
+        key: String!
+        answer: Int = 42
+      }
+
+      input AnnotatedInput @onInputObject {
+        annotatedField: Type @onInputFieldDefinition
+      }
+
+      input UndefinedInput
+
+      extend input InputType {
+        other: Float = 1.23e4 @onInputFieldDefinition
+      }
+
+      extend input InputType @onInputObject
+
+      """
+      This is a description of the \`@skip\` directive
+      """
+      directive @skip(if: Boolean! @onArgumentDefinition) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
+
+      directive @include(if: Boolean!) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
+
+      directive @include2(if: Boolean!) on FIELD | FRAGMENT_SPREAD | INLINE_FRAGMENT
+
+      extend schema @onSchema
+
+      extend schema @onSchema {
+        subscription: SubscriptionType
+      }
+    `;
+
+    expect(printed).to.equal(result);
+
+    const printedStripped = stripIgnoredTokens(print(ast));
+
+    console.log(printedStripped);
+
+    const resultStripped = `schema{query:QueryType mutation:MutationType}"""
+This is a description
+of the \`Foo\` type.
+""" type Foo implements Bar&Baz{"Description of the \`one\` field." one:Type """
+  This is a description of the \`two\` field.
+  """ two("""
+    This is a description of the \`argument\` argument.
+    """ argument:InputType!):Type """
+  This is a description of the \`three\` field.
+  """ three(argument:InputType other:String):Int four(argument:String="string"):String five(argument:[String]=["string" "string"]):String six(argument:InputType={key:"value"}):Type seven(argument:Int=null):Type}type AnnotatedObject@onObject(arg:"value"){annotatedField(arg:Type="default"@onArgumentDefinition):Type@onField}type UndefinedType extend type Foo{seven(argument:[String]):Type}extend type Foo@onType interface Bar{one:Type four(argument:String="string"):String}interface AnnotatedInterface@onInterface{annotatedField(arg:Type@onArgumentDefinition):Type@onField}interface UndefinedInterface extend interface Bar{two(argument:InputType!):Type}extend interface Bar@onInterface union Feed=Story|Article|Advert union AnnotatedUnion@onUnion=A|B union AnnotatedUnionTwo@onUnion=A|B union UndefinedUnion extend union Feed=Photo|Video extend union Feed@onUnion scalar CustomScalar scalar AnnotatedScalar@onScalar extend scalar CustomScalar@onScalar enum Site{"""
+  This is a description of the \`DESKTOP\` value
+  """ DESKTOP """
+  This is a description of the \`MOBILE\` value
+  """ MOBILE "This is a description of the \`WEB\` value" WEB}enum AnnotatedEnum@onEnum{ANNOTATED_VALUE@onEnumValue OTHER_VALUE}enum UndefinedEnum extend enum Site{VR}extend enum Site@onEnum input InputType{key:String!answer:Int=42}input AnnotatedInput@onInputObject{annotatedField:Type@onInputFieldDefinition}input UndefinedInput extend input InputType{other:Float=1.23e4@onInputFieldDefinition}extend input InputType@onInputObject """
+This is a description of the \`@skip\` directive
+""" directive@skip(if:Boolean!@onArgumentDefinition)on FIELD|FRAGMENT_SPREAD|INLINE_FRAGMENT directive@include(if:Boolean!)on FIELD|FRAGMENT_SPREAD|INLINE_FRAGMENT directive@include2(if:Boolean!)on FIELD|FRAGMENT_SPREAD|INLINE_FRAGMENT extend schema@onSchema extend schema@onSchema{subscription:SubscriptionType}`;
 
     expect(printedStripped).to.equal(resultStripped);
 
