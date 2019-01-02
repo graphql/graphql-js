942481149c0dd8fa089c5d05003124add086e578
Ryan Bonte <ryan.bonte@gmail.com>
2019-01-02 21:47:35 +0100

    cleanup

--- src/utilities/__tests__/stripIgnoredTokens-test.js
+++ src/utilities/__tests__/stripIgnoredTokens-test.js
@@ -265,7 +265,7 @@ describe('Strip ignored tokens: Query document', () => {
 
     expect(printed).to.equal(result);
 
-    const printedStripped = stripIgnoredTokens(print(ast, { condense: true }));
+    const printedStripped = stripIgnoredTokens(print(ast));
 
     const resultStripped = String.raw`query queryName($foo:ComplexType$site:Site=MOBILE)@onQuery{whoever123is:node(id:[123 456]){id...on User@onInlineFragment{field2{id alias:field1(first:10 after:$foo)@include(if:$foo){id...frag@onFragmentSpread}}}...@skip(unless:$foo){id}...{id}}}mutation likeStory@onMutation{like(story:123)@onField{story{id@onField}}}subscription StoryLikeSubscription($input:StoryLikeSubscribeInput)@onSubscription{storyLikeSubscribe(input:$input){story{likers{count}likeSentence{text}}}}fragment frag on Friend@onFragmentDefinition{foo(size:$size bar:$b obj:{key:"value" block:"""
     block string uses \"""
