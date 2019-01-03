9374440a97400efb16e6ba7fd05e5eb837bd47bb
Ryan Bonte <ryan.bonte@gmail.com>
2019-01-03 17:42:19 +0100

    typo

--- src/utilities/stripIgnoredTokens.js
+++ src/utilities/stripIgnoredTokens.js
@@ -15,7 +15,7 @@ const slice = String.prototype.slice;
 
 /**
  * Strips tokens that are not significant to the validity of a GraphQL string.
- * This can be used to save on bundle size at compile-time and network payload
+ * This can be used to save on bundle size at compile time and network payload
  * size at runtime.
  *
  * Example:
