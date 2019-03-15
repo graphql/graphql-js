75d301c9ac1a0f68b5607e47e9470618fd5c98c8
Travis CI <github@fb.com>
2019-03-15 14:57:32 +0000

    Deploy ffccf3f3362c304fd18e2132609dbc7e8d94e968 to NPM branch

--- jsutils/inspect.js
+++ jsutils/inspect.js
@@ -11,10 +11,17 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+var MAX_ARRAY_LENGTH = 10;
+var MAX_RECURSIVE_DEPTH = 2;
 /**
  * Used to print values in error messages.
  */
+
 function inspect(value) {
+  return formatValue(value, []);
+}
+
+function formatValue(value, seenValues) {
   switch (_typeof(value)) {
     case 'string':
       return JSON.stringify(value);
@@ -23,28 +30,82 @@ function inspect(value) {
       return value.name ? "[function ".concat(value.name, "]") : '[function]';
 
     case 'object':
-      if (value) {
-        var customInspectFn = getCustomFn(value);
-
-        if (customInspectFn) {
-          // $FlowFixMe(>=0.90.0)
-          var customValue = customInspectFn.call(value);
-          return typeof customValue === 'string' ? customValue : inspect(customValue);
-        } else if (Array.isArray(value)) {
-          return '[' + value.map(inspect).join(', ') + ']';
-        }
-
-        var properties = Object.keys(value).map(function (k) {
-          return "".concat(k, ": ").concat(inspect(value[k]));
-        }).join(', ');
-        return properties ? '{ ' + properties + ' }' : '{}';
-      }
-
-      return String(value);
+      return formatObjectValue(value, seenValues);
 
     default:
       return String(value);
   }
+}
+
+function formatObjectValue(value, previouslySeenValues) {
+  if (previouslySeenValues.indexOf(value) !== -1) {
+    return '[Circular]';
+  }
+
+  var seenValues = [].concat(previouslySeenValues, [value]);
+
+  if (value) {
+    var customInspectFn = getCustomFn(value);
+
+    if (customInspectFn) {
+      // $FlowFixMe(>=0.90.0)
+      var customValue = customInspectFn.call(value); // check for infinite recursion
+
+      if (customValue !== value) {
+        return typeof customValue === 'string' ? customValue : formatValue(customValue, seenValues);
+      }
+    } else if (Array.isArray(value)) {
+      return formatArray(value, seenValues);
+    }
+
+    return formatObject(value, seenValues);
+  }
+
+  return String(value);
+}
+
+function formatObject(object, seenValues) {
+  var keys = Object.keys(object);
+
+  if (keys.length === 0) {
+    return '{}';
+  }
+
+  if (seenValues.length > MAX_RECURSIVE_DEPTH) {
+    return '[' + getObjectTag(object) + ']';
+  }
+
+  var properties = keys.map(function (key) {
+    var value = formatValue(object[key], seenValues);
+    return key + ': ' + value;
+  });
+  return '{ ' + properties.join(', ') + ' }';
+}
+
+function formatArray(array, seenValues) {
+  if (array.length === 0) {
+    return '[]';
+  }
+
+  if (seenValues.length > MAX_RECURSIVE_DEPTH) {
+    return '[Array]';
+  }
+
+  var len = Math.min(MAX_ARRAY_LENGTH, array.length);
+  var remaining = array.length - len;
+  var items = [];
+
+  for (var i = 0; i < len; ++i) {
+    items.push(formatValue(array[i], seenValues));
+  }
+
+  if (remaining === 1) {
+    items.push('... 1 more item');
+  } else if (remaining > 1) {
+    items.push("... ".concat(remaining, " more items"));
+  }
+
+  return '[' + items.join(', ') + ']';
 }
 
 function getCustomFn(object) {
@@ -57,4 +118,18 @@ function getCustomFn(object) {
   if (typeof object.inspect === 'function') {
     return object.inspect;
   }
+}
+
+function getObjectTag(object) {
+  var tag = Object.prototype.toString.call(object).replace(/^\[object /, '').replace(/]$/, '');
+
+  if (tag === 'Object' && typeof object.constructor === 'function') {
+    var name = object.constructor.name;
+
+    if (typeof name === 'string') {
+      return name;
+    }
+  }
+
+  return tag;
 }
