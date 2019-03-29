c1376e3aa113c118c17115e17978dd6d86ab6ffe
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-03-29 13:09:12 +0200

    Enable Flow on more test files (#1806)

--- src/subscription/__tests__/eventEmitterAsyncIterator-test.js
+++ src/subscription/__tests__/eventEmitterAsyncIterator-test.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @noflow
+ * @flow strict
  */
 
 import { expect } from 'chai';
@@ -49,6 +49,7 @@ describe('eventEmitterAsyncIterator', () => {
     const i5 = iterator.next().then(x => x);
 
     // Terminate emitter
+    // $FlowFixMe
     await iterator.return();
 
     // Publish is not caught after terminate
