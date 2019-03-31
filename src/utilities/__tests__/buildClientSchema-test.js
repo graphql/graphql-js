graphql/graphql-js

commit 183ff32bee4bc23eb23657f79f414c2400ecb06a

Remove excessive cache inside buildClientSchema (#1677)

src/utilities/buildClientSchema.js
@@ -7,9 +7,9 @@
  * @flow strict
  */
 
+import objectValues from '../polyfills/objectValues';
 import inspect from '../jsutils/inspect';
 import invariant from '../jsutils/invariant';
-import keyMap from '../jsutils/keyMap';
 import keyValMap from '../jsutils/keyValMap';
 import { valueFromAST } from './valueFromAST';
 import { parseValue } from '../language/parser';
@@ -84,19 +84,18 @@ export function buildClientSchema(
   // Get the schema from the introspection result.
   const schemaIntrospection = introspection.__schema;
 
-  // Converts the list of types into a keyMap based on the type names.
-  const typeIntrospectionMap = keyMap(
+  // Iterate through all types, getting the type definition for each.
+  const typeMap = keyValMap(
     schemaIntrospection.types,
-    type => type.name,
+    typeIntrospection => typeIntrospection.name,
+    typeIntrospection => buildType(typeIntrospection),
   );
 
-  // A cache to use to store the actual GraphQLType definition objects by name.
-  // Initialize to the GraphQL built in scalars. All functions below are inline
-  // so that this type def cache is within the scope of the closure.
-  const typeDefCache = keyMap(
-    specifiedScalarTypes.concat(introspectionTypes),
-    type => type.name,
-  );
+  for (const stdType of [...specifiedScalarTypes, ...introspectionTypes]) {
+    if (typeMap[stdType.name]) {
+      typeMap[stdType.name] = stdType;
+    }
+  }
 
   // Given a type reference in introspection, return the GraphQLType instance.
   // preferring cached instances before building new instances.
@@ -123,20 +122,16 @@ export function buildClientSchema(
   }
 
   function getNamedType(typeName: string): GraphQLNamedType {
-    if (typeDefCache[typeName]) {
-      return typeDefCache[typeName];
-    }
-    const typeIntrospection = typeIntrospectionMap[typeName];
-    if (!typeIntrospection) {
+    const type = typeMap[typeName];
+    if (!type) {
       throw new Error(
         `Invalid or incomplete schema, unknown type: ${typeName}. Ensure ` +
           'that a full introspection query is used in order to build a ' +
           'client schema.',
       );
     }
-    const typeDef = buildType(typeIntrospection);
-    typeDefCache[typeName] = typeDef;
-    return typeDef;
+
+    return type;
   }
 
   function getInputType(typeRef: IntrospectionInputTypeRef): GraphQLInputType {
@@ -362,12 +357,6 @@ export function buildClientSchema(
     });
   }
 
-  // Iterate through all types, getting the type definition for each, ensuring
-  // that any type not directly referenced by a field will get created.
-  const types = schemaIntrospection.types.map(typeIntrospection =>
-    getNamedType(typeIntrospection.name),
-  );
-
   // Get the root Query, Mutation, and Subscription types.
   const queryType = schemaIntrospection.queryType
     ? getObjectType(schemaIntrospection.queryType)
@@ -392,7 +381,7 @@ export function buildClientSchema(
     query: queryType,
     mutation: mutationType,
     subscription: subscriptionType,
-    types,
+    types: objectValues(typeMap),
     directives,
     assumeValid: options && options.assumeValid,
     allowedLegacyNames: options && options.allowedLegacyNames,

commit 52050d53aa54ba35bf9ec8c7482d055fcfd966a0

buildClientSchema: Revert breaking change introduced in #1677
More details here: https://github.com/graphql/graphql-js/commit/183ff32bee4bc23eb23657f79f414c2400ecb06a#r32971387

src/utilities/buildClientSchema.js
@@ -92,9 +92,7 @@ export function buildClientSchema(
   );
 
   for (const stdType of [...specifiedScalarTypes, ...introspectionTypes]) {
-    if (typeMap[stdType.name]) {
-      typeMap[stdType.name] = stdType;
-    }
+    typeMap[stdType.name] = stdType;
   }
 
   // Get the root Query, Mutation, and Subscription types.

src/utilities/__tests__/buildClientSchema-test.js
@@ -38,15 +38,25 @@ function cycleIntrospection(sdlString) {
   const serverSchema = buildSchema(sdlString);
   const initialIntrospection = introspectionFromSchema(serverSchema);
   const clientSchema = buildClientSchema(initialIntrospection);
+  const secondIntrospection = introspectionFromSchema(clientSchema);
+
+  hackToRemoveStandardTypes(secondIntrospection);
+  hackToRemoveStandardTypes(initialIntrospection);
 
   /**
    * If the client then runs the introspection query against the client-side
    * schema, it should get a result identical to what was returned by the server
    */
-  const secondIntrospection = introspectionFromSchema(clientSchema);
   expect(secondIntrospection).to.deep.equal(initialIntrospection);
-
   return printSchema(clientSchema);
+}
+
+// Temporary hack to remove always presented standard types should be removed in 15.0
+function hackToRemoveStandardTypes(introspection) {
+  (introspection.__schema: any).types = introspection.__schema.types.filter(
+    ({ name }) =>
+      ['ID', 'Float', 'Int', 'Boolean', 'String'].indexOf(name) === -1,
+  );
 }
 
 describe('Type System: build schema from introspection', () => {
@@ -320,6 +330,9 @@ describe('Type System: build schema from introspection', () => {
     const introspection = introspectionFromSchema(schema);
     const clientSchema = buildClientSchema(introspection);
     const secondIntrospection = introspectionFromSchema(clientSchema);
+
+    hackToRemoveStandardTypes(secondIntrospection);
+    hackToRemoveStandardTypes(introspection);
     expect(secondIntrospection).to.deep.equal(introspection);
 
     const clientFoodEnum = clientSchema.getType('Food');