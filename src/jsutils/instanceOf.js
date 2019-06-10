35c68d403cf1020e1d30ea29f0546352d03dd49e
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-06-11 00:04:00 +0300

    Update LICENSE and remove license header from source files (#1960)

--- src/jsutils/instanceOf.js
+++ src/jsutils/instanceOf.js
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow strict
- */
+// @flow strict
 
 /**
  * A replacement for instanceof which includes an error warning when multi-realm
--- src/jsutils/isPromise.js
+++ src/jsutils/isPromise.js
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow strict
- */
+// @flow strict
 
 /**
  * Returns true if the value acts like a Promise, i.e. has a "then" function,
