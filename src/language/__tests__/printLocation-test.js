d8c1dfdc9dbbdef2400363cb0748d50cbeef39a8
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-06-14 17:30:35 +0300

    printLocation: Add special support for minified documents

--- src/language/__tests__/printLocation-test.js
+++ src/language/__tests__/printLocation-test.js
@@ -7,7 +7,48 @@ import dedent from '../../jsutils/dedent';
 import { Source } from '../../language';
 import { printSourceLocation } from '../printLocation';
 
-describe('printLocation', () => {
+describe('printSourceLocation', () => {
+  it('prints minified documents', () => {
+    const minifiedSource = new Source(
+      'query SomeMiniFiedQueryWithErrorInside($foo:String!=FIRST_ERROR_HERE$bar:String){someField(foo:$foo bar:$bar baz:SECOND_ERROR_HERE){fieldA fieldB{fieldC fieldD...on THIRD_ERROR_HERE}}}',
+    );
+
+    const firstLocation = printSourceLocation(minifiedSource, {
+      line: 1,
+      column: minifiedSource.body.indexOf('FIRST_ERROR_HERE') + 1,
+    });
+    expect(firstLocation + '\n').to.equal(dedent`
+      GraphQL request:1:53
+      1 | query SomeMiniFiedQueryWithErrorInside($foo:String!=FIRST_ERROR_HERE$bar:String)
+        |                                                     ^
+        | {someField(foo:$foo bar:$bar baz:SECOND_ERROR_HERE){fieldA fieldB{fieldC fieldD.
+    `);
+
+    const secondLocation = printSourceLocation(minifiedSource, {
+      line: 1,
+      column: minifiedSource.body.indexOf('SECOND_ERROR_HERE') + 1,
+    });
+    expect(secondLocation + '\n').to.equal(dedent`
+      GraphQL request:1:114
+      1 | query SomeMiniFiedQueryWithErrorInside($foo:String!=FIRST_ERROR_HERE$bar:String)
+        | {someField(foo:$foo bar:$bar baz:SECOND_ERROR_HERE){fieldA fieldB{fieldC fieldD.
+        |                                  ^
+        | ..on THIRD_ERROR_HERE}}}
+    `);
+
+    const thirdLocation = printSourceLocation(minifiedSource, {
+      line: 1,
+      column: minifiedSource.body.indexOf('THIRD_ERROR_HERE') + 1,
+    });
+    expect(thirdLocation + '\n').to.equal(dedent`
+      GraphQL request:1:166
+      1 | query SomeMiniFiedQueryWithErrorInside($foo:String!=FIRST_ERROR_HERE$bar:String)
+        | {someField(foo:$foo bar:$bar baz:SECOND_ERROR_HERE){fieldA fieldB{fieldC fieldD.
+        | ..on THIRD_ERROR_HERE}}}
+        |      ^
+    `);
+  });
+
   it('prints single digit line number with no padding', () => {
     const result = printSourceLocation(
       new Source('*', 'Test', { line: 9, column: 1 }),
