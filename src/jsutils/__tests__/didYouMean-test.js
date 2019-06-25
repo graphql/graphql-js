b904859fbd6059be8d61e2357dd0f0c0cbab4d1f
Christoph Zwerschke <cito@online.de>
2019-06-25 13:50:21 +0200

    Add test for didYouMean with sub-message (#1988)

--- src/jsutils/__tests__/didYouMean-test.js
+++ src/jsutils/__tests__/didYouMean-test.js
@@ -9,21 +9,27 @@ describe('didYouMean', () => {
     expect(didYouMean([])).to.equal('');
   });
 
-  it('Handle single suggestion', () => {
+  it('Handles single suggestion', () => {
     expect(didYouMean(['A'])).to.equal(' Did you mean A?');
   });
 
-  it('Handle two suggestions', () => {
+  it('Handles two suggestions', () => {
     expect(didYouMean(['A', 'B'])).to.equal(' Did you mean A or B?');
   });
 
-  it('Handle multiple suggestions', () => {
+  it('Handles multiple suggestions', () => {
     expect(didYouMean(['A', 'B', 'C'])).to.equal(' Did you mean A, B, or C?');
   });
 
   it('Limits to five suggestions', () => {
     expect(didYouMean(['A', 'B', 'C', 'D', 'E', 'F'])).to.equal(
       ' Did you mean A, B, C, D, or E?',
+    );
+  });
+
+  it('Adds sub-message', () => {
+    expect(didYouMean('the letter', ['A'])).to.equal(
+      ' Did you mean the letter A?',
     );
   });
 });
