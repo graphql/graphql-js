88039443b48c864de95f0986a149c4d81438b81f
Travis CI <github@fb.com>
2019-06-27 09:54:52 +0000

    Deploy 1710fb0bbb83c1574fda78a2af6bbb804d5d1770 to NPM branch

--- version.js
+++ version.js
@@ -13,7 +13,7 @@ exports.versionInfo = exports.version = void 0;
 /**
  * A string containing the version of the GraphQL.js library
  */
-var version = '14.3.1';
+var version = '14.4.0';
 /**
  * An object containing the components of the GraphQL.js version string
  */
@@ -21,8 +21,8 @@ var version = '14.3.1';
 exports.version = version;
 var versionInfo = Object.freeze({
   major: 14,
-  minor: 3,
-  patch: 1,
+  minor: 4,
+  patch: 0,
   preReleaseTag: null
 });
 exports.versionInfo = versionInfo;
--- version.js.flow
+++ version.js.flow
@@ -8,14 +8,14 @@
 /**
  * A string containing the version of the GraphQL.js library
  */
-export const version = '14.3.1';
+export const version = '14.4.0';
 
 /**
  * An object containing the components of the GraphQL.js version string
  */
 export const versionInfo = Object.freeze({
   major: 14,
-  minor: 3,
-  patch: 1,
+  minor: 4,
+  patch: 0,
   preReleaseTag: null,
 });
