12317c577596b32a879d82a9e2c0a1015bed5319
Travis CI <github@fb.com>
2019-06-28 19:39:59 +0000

    Deploy 27f695eef5c22a87ad8fdd290eaa61aec4ed7f40 to NPM branch

--- jsutils/dedent.mjs
+++ jsutils/dedent.mjs
@@ -1,18 +1,3 @@
-import invariant from './invariant';
-/**
- * fixes indentation by removing leading spaces and tabs from each line
- */
-
-function fixIndent(str) {
-  var trimmedStr = str.replace(/^\n*/m, '') //  remove leading newline
-  .replace(/[ \t]*$/, ''); // remove trailing spaces and tabs
-
-  var indentMatch = /^[ \t]*/.exec(trimmedStr);
-  !Array.isArray(indentMatch) ? invariant(0) : void 0;
-  var indent = indentMatch[0]; // figure out indent
-
-  return trimmedStr.replace(RegExp('^' + indent, 'mg'), ''); // remove indent
-}
 /**
  * An ES6 string tag that fixes indentation. Also removes leading newlines
  * and trailing spaces and tabs, but keeps trailing newlines.
@@ -25,24 +10,50 @@ function fixIndent(str) {
  * `;
  * str === "{\n  test\n}\n";
  */
-
-
 export default function dedent(strings) {
-  for (var _len = arguments.length, values = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    values[_key - 1] = arguments[_key];
+  var str = '';
+
+  for (var i = 0; i < strings.length; ++i) {
+    str += strings[i];
+
+    if (i < (arguments.length <= 1 ? 0 : arguments.length - 1)) {
+      str += i + 1 < 1 || arguments.length <= i + 1 ? undefined : arguments[i + 1]; // interpolation
+    }
   }
 
-  // when used as an ordinary function, allow passing a singleton string
-  var strArray = typeof strings === 'string' ? [strings] : strings;
-  var numValues = values.length;
-  var str = strArray.reduce(function (prev, cur, index) {
-    var next = prev + cur;
+  var trimmedStr = str.replace(/^\n*/m, '') //  remove leading newline
+  .replace(/[ \t]*$/, ''); // remove trailing spaces and tabs
+  // fixes indentation by removing leading spaces and tabs from each line
 
-    if (index < numValues) {
-      next += values[index]; // interpolation
+  var indent = '';
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = trimmedStr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var char = _step.value;
+
+      if (char !== ' ' && char !== '\t') {
+        break;
+      }
+
+      indent += char;
     }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return != null) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
 
-    return next;
-  }, '');
-  return fixIndent(str);
+  return trimmedStr.replace(RegExp('^' + indent, 'mg'), ''); // remove indent
 }
