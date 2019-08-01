graphql/graphql-js

commit a5bbe713f4b704247d56616e522f3d855354b974

Convert all non-reachable exceptions into invariants (#2067)

resources/inline-invariant.js
@@ -21,6 +21,7 @@ module.exports = function inlineInvariant(context) {
     (%%cond%%) || devAssert(0, %%args%%)
   `);
 
+  const t = context.types;
   return {
     visitor: {
       CallExpression(path) {
@@ -38,13 +39,35 @@ module.exports = function inlineInvariant(context) {
         const calleeName = node.callee.name;
         if (calleeName === 'invariant') {
           const [cond, args] = node.arguments;
-          path.addComment('leading', ' istanbul ignore next ');
-          path.replaceWith(invariantTemplate({ cond, args }));
+
+          // Check if it is unreachable invariant: "invariant(false, ...)"
+          if (cond.type === 'BooleanLiteral' && cond.value === false) {
+            addIstanbulIgnoreElse(path);
+          } else {
+            path.replaceWith(invariantTemplate({ cond, args }));
+          }
         } else if (calleeName === 'devAssert') {
           const [cond, args] = node.arguments;
           path.replaceWith(assertTemplate({ cond, args }));
         }
+
+        path.addComment('leading', ' istanbul ignore next ');
       },
     },
   };
+
+  function addIstanbulIgnoreElse(path) {
+    const parentStatement = path.getStatementParent();
+    const previousStatement =
+      parentStatement.container[parentStatement.key - 1];
+    if (previousStatement.type === 'IfStatement') {
+      let lastIf = previousStatement;
+      while (lastIf.alternate && lastIf.alternate.type === 'IfStatement') {
+        lastIf = lastIf.alternate;
+      }
+      if (lastIf.alternate == null) {
+        t.addComment(lastIf, 'leading', ' istanbul ignore else ');
+      }
+    }
+  }
 };

src/execution/execute.js
@@ -4,6 +4,7 @@ import { forEach, isCollection } from 'iterall';
 
 import inspect from '../jsutils/inspect';
 import memoize3 from '../jsutils/memoize3';
+import invariant from '../jsutils/invariant';
 import devAssert from '../jsutils/devAssert';
 import isInvalid from '../jsutils/isInvalid';
 import isNullish from '../jsutils/isNullish';
@@ -872,11 +873,10 @@ function completeValue(
   }
 
   // Not reachable. All possible output types have been considered.
-  /* istanbul ignore next */
-  throw new Error(
-    `Cannot complete value of unexpected output type: "${inspect(
-      (returnType: empty),
-    )}".`,
+  invariant(
+    false,
+    'Cannot complete value of unexpected output type: ' +
+      inspect((returnType: empty)),
   );
 }
 

src/type/introspection.js
@@ -3,6 +3,7 @@
 import objectValues from '../polyfills/objectValues';
 
 import inspect from '../jsutils/inspect';
+import invariant from '../jsutils/invariant';
 
 import { print } from '../language/printer';
 import { DirectiveLocation } from '../language/directiveLocation';
@@ -208,8 +209,7 @@ export const __Type = new GraphQLObjectType({
           }
 
           // Not reachable. All possible types have been considered.
-          /* istanbul ignore next */
-          throw new Error(`Unexpected type: "${inspect((type: empty))}".`);
+          invariant(false, `Unexpected type: "${inspect((type: empty))}".`);
         },
       },
       name: {

src/utilities/astFromValue.js
@@ -5,6 +5,7 @@ import { forEach, isCollection } from 'iterall';
 import objectValues from '../polyfills/objectValues';
 
 import inspect from '../jsutils/inspect';
+import invariant from '../jsutils/invariant';
 import isNullish from '../jsutils/isNullish';
 import isInvalid from '../jsutils/isInvalid';
 import isObjectLike from '../jsutils/isObjectLike';
@@ -137,8 +138,7 @@ export function astFromValue(value: mixed, type: GraphQLInputType): ?ValueNode {
   }
 
   // Not reachable. All possible input types have been considered.
-  /* istanbul ignore next */
-  throw new Error(`Unexpected input type: "${inspect((type: empty))}".`);
+  invariant(false, 'Unexpected input type: ' + inspect((type: empty)));
 }
 
 /**

src/utilities/buildASTSchema.js
@@ -4,6 +4,7 @@ import objectValues from '../polyfills/objectValues';
 
 import keyMap from '../jsutils/keyMap';
 import inspect from '../jsutils/inspect';
+import invariant from '../jsutils/invariant';
 import devAssert from '../jsutils/devAssert';
 import keyValMap from '../jsutils/keyValMap';
 import { type ObjMap } from '../jsutils/ObjMap';
@@ -313,9 +314,9 @@ export class ASTDefinitionBuilder {
     }
 
     // Not reachable. All possible type definition nodes have been considered.
-    /* istanbul ignore next */
-    throw new Error(
-      `Unexpected type definition node: "${inspect((astNode: empty))}".`,
+    invariant(
+      false,
+      'Unexpected type definition node: ' + inspect((astNode: empty)),
     );
   }
 

src/utilities/coerceValue.js
@@ -5,6 +5,7 @@ import { forEach, isCollection } from 'iterall';
 import objectValues from '../polyfills/objectValues';
 
 import inspect from '../jsutils/inspect';
+import invariant from '../jsutils/invariant';
 import didYouMean from '../jsutils/didYouMean';
 import isObjectLike from '../jsutils/isObjectLike';
 import suggestionList from '../jsutils/suggestionList';
@@ -195,8 +196,7 @@ export function coerceValue(
   }
 
   // Not reachable. All possible input types have been considered.
-  /* istanbul ignore next */
-  throw new Error(`Unexpected input type: "${inspect((type: empty))}".`);
+  invariant(false, 'Unexpected input type: ' + inspect((type: empty)));
 }
 
 function ofValue(value) {

src/utilities/extendSchema.js
@@ -5,6 +5,7 @@ import objectValues from '../polyfills/objectValues';
 
 import inspect from '../jsutils/inspect';
 import mapValue from '../jsutils/mapValue';
+import invariant from '../jsutils/invariant';
 import devAssert from '../jsutils/devAssert';
 import keyValMap from '../jsutils/keyValMap';
 
@@ -258,8 +259,7 @@ export function extendSchema(
     }
 
     // Not reachable. All possible types have been considered.
-    /* istanbul ignore next */
-    throw new Error(`Unexpected type: "${inspect((type: empty))}".`);
+    invariant(false, 'Unexpected type: ' + inspect((type: empty)));
   }
 
   function extendDirective(directive: GraphQLDirective): GraphQLDirective {

src/utilities/findBreakingChanges.js
@@ -512,8 +512,7 @@ function typeKindName(type: GraphQLNamedType): string {
   }
 
   // Not reachable. All possible named types have been considered.
-  /* istanbul ignore next */
-  throw new TypeError(`Unexpected type: ${inspect((type: empty))}.`);
+  invariant(false, 'Unexpected type: ' + inspect((type: empty)));
 }
 
 function stringifyValue(value: mixed, type: GraphQLInputType): string {

commit 31083c73db69d95e9d4111b6f5ee4d668a289e78

coverage: Fixed coverage bug introduced in #2067

resources/inline-invariant.js
@@ -46,12 +46,11 @@ module.exports = function inlineInvariant(context) {
           } else {
             path.replaceWith(invariantTemplate({ cond, args }));
           }
+          path.addComment('leading', ' istanbul ignore next ');
         } else if (calleeName === 'devAssert') {
           const [cond, args] = node.arguments;
           path.replaceWith(assertTemplate({ cond, args }));
         }
-
-        path.addComment('leading', ' istanbul ignore next ');
       },
     },
   };
@@ -60,14 +59,11 @@ module.exports = function inlineInvariant(context) {
     const parentStatement = path.getStatementParent();
     const previousStatement =
       parentStatement.container[parentStatement.key - 1];
-    if (previousStatement.type === 'IfStatement') {
-      let lastIf = previousStatement;
-      while (lastIf.alternate && lastIf.alternate.type === 'IfStatement') {
-        lastIf = lastIf.alternate;
-      }
-      if (lastIf.alternate == null) {
-        t.addComment(lastIf, 'leading', ' istanbul ignore else ');
-      }
+    if (
+      previousStatement.type === 'IfStatement' &&
+      previousStatement.alternate == null
+    ) {
+      t.addComment(previousStatement, 'leading', ' istanbul ignore else ');
     }
   }
 };