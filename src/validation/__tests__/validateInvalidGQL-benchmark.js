d697f28add6f760d61d8e74268930aadb5e96c28
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-08-08 00:45:58 +0300

    Add benchmark for validating invalid query
    Motivated by #2074

---
+++ src/validation/__tests__/validateInvalidGQL-benchmark.js
@@ -0,0 +1,29 @@
+// @flow strict
+
+import { parse } from '../../language/parser';
+import { buildSchema } from '../../utilities/buildASTSchema';
+
+import { validate } from '../validate';
+
+import { bigSchemaSDL } from '../../__fixtures__';
+
+const schema = buildSchema(bigSchemaSDL, { assumeValid: true });
+const queryAST = parse(`
+  {
+    unknownField
+    ... on unknownType {
+      anotherUnknownField
+      ...unknownFragment
+    }
+  }
+
+  fragment TestFragment on anotherUnknownType {
+    yetAnotherUnknownField
+  }
+`);
+
+export const name = 'Validate Invalid Query';
+export const count = 50;
+export function measure() {
+  validate(schema, queryAST);
+}
