ada8f874e71243626063c41d01b079bf9ffc1a70
Jackson Kearl <jackson.kearl@apollographql.com>
2019-08-23 10:07:35 -0700

    `unknown` => `any`, to target TS@2.6

--- tstypes/type/scalars.d.ts
+++ tstypes/type/scalars.d.ts
@@ -8,4 +8,4 @@ export const GraphQLID: GraphQLScalarType;
 
 export const specifiedScalarTypes: ReadonlyArray<GraphQLScalarType>;
 
-export function isSpecifiedScalarType(type: unknown): type is GraphQLScalarType;
+export function isSpecifiedScalarType(type: any): type is GraphQLScalarType;
