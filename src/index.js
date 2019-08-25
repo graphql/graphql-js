c072f559ecdd92edf76442f40ce7534f084a2bdf
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-08-25 13:24:48 +0300

    Remove deprecated 'isValidLiteralValue' function

--- src/index.js
+++ src/index.js
@@ -387,8 +387,6 @@ export {
   coerceInputValue,
   // @deprecated use coerceInputValue - will be removed in v15
   coerceValue,
-  // @deprecated use validation - will be removed in v15
-  isValidLiteralValue,
   // Concatenates multiple AST together.
   concatAST,
   // Separates an AST into an AST per Operation.
