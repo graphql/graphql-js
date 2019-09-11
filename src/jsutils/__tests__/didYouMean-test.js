cab0358b2d3126c2037276d28b1279665bacaf60
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2019-09-11 20:37:15 +0300

    didYouMean: quote suggested values

--- src/jsutils/__tests__/didYouMean-test.js
+++ src/jsutils/__tests__/didYouMean-test.js
@@ -11,26 +11,28 @@ describe('didYouMean', () => {
   });
 
   it('Handles single suggestion', () => {
-    expect(didYouMean(['A'])).to.equal(' Did you mean A?');
+    expect(didYouMean(['A'])).to.equal(' Did you mean "A"?');
   });
 
   it('Handles two suggestions', () => {
-    expect(didYouMean(['A', 'B'])).to.equal(' Did you mean A or B?');
+    expect(didYouMean(['A', 'B'])).to.equal(' Did you mean "A" or "B"?');
   });
 
   it('Handles multiple suggestions', () => {
-    expect(didYouMean(['A', 'B', 'C'])).to.equal(' Did you mean A, B, or C?');
+    expect(didYouMean(['A', 'B', 'C'])).to.equal(
+      ' Did you mean "A", "B", or "C"?',
+    );
   });
 
   it('Limits to five suggestions', () => {
     expect(didYouMean(['A', 'B', 'C', 'D', 'E', 'F'])).to.equal(
-      ' Did you mean A, B, C, D, or E?',
+      ' Did you mean "A", "B", "C", "D", or "E"?',
     );
   });
 
   it('Adds sub-message', () => {
     expect(didYouMean('the letter', ['A'])).to.equal(
-      ' Did you mean the letter A?',
+      ' Did you mean the letter "A"?',
     );
   });
 });
