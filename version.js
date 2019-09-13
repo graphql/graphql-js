ec2b69228f23c3fe38125d5f3e91623d7e7b7735
Travis CI <github@fb.com>
2019-09-13 15:29:19 +0000

    Deploy 38bd33707d2ca02cc07dd5aee70f027a2ea75525 to NPM branch

--- version.js
+++ version.js
@@ -13,7 +13,7 @@ exports.versionInfo = exports.version = void 0;
 /**
  * A string containing the version of the GraphQL.js library
  */
-var version = '14.5.4';
+var version = '14.5.5';
 /**
  * An object containing the components of the GraphQL.js version string
  */
@@ -22,7 +22,7 @@ exports.version = version;
 var versionInfo = Object.freeze({
   major: 14,
   minor: 5,
-  patch: 4,
+  patch: 5,
   preReleaseTag: null
 });
 exports.versionInfo = versionInfo;
