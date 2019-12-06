316a69e39d3da3e441b80ec068b0ec07f85616ec
lukejacksonn <lukejacksonn@gmail.com>
2019-12-06 15:51:12 +0000

    Implements export statement rewriting

--- .babelrc.js
+++ .babelrc.js
@@ -22,10 +22,24 @@ module.exports = {
           return {
             visitor: {
               ImportDeclaration: function(path, state) {
+                if (!path.node.source) return;
                 var source = path.node.source.value;
                 if (source.match(/^\.{0,2}\//) && !source.endsWith('.es.js')) {
                   path.replaceWith(
                     types.importDeclaration(
+                      path.node.specifiers,
+                      types.stringLiteral(source + '.es.js'),
+                    ),
+                  );
+                }
+              },
+              ExportNamedDeclaration: function(path, state) {
+                if (!path.node.source) return;
+                const source = path.node.source.value;
+                if (source.match(/^\.{0,2}\//) && !source.endsWith('.es.js')) {
+                  path.replaceWith(
+                    types.exportNamedDeclaration(
+                      path.node.declaration,
                       path.node.specifiers,
                       types.stringLiteral(source + '.es.js'),
                     ),
