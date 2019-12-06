8e7cc02775e77c3603049c58115540da2e350d1f
lukejacksonn <lukejacksonn@gmail.com>
2019-12-06 12:18:12 +0000

    Checks typeof process in instanceOf before using it

--- src/jsutils/instanceOf.js
+++ src/jsutils/instanceOf.js
@@ -11,7 +11,8 @@ declare function instanceOf(
 
 // See: https://expressjs.com/en/advanced/best-practice-performance.html#set-node_env-to-production
 // See: https://webpack.js.org/guides/production/
-export default process.env.NODE_ENV === 'production'
+export default typeof process !== 'undefined' &&
+process.env.NODE_ENV === 'production'
   ? // eslint-disable-next-line no-shadow
     function instanceOf(value: mixed, constructor: mixed) {
       return value instanceof constructor;
