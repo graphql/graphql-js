642eeafdbea65a5c8f5238547e23cadb31264074
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-01-04 18:23:26 +0700

    Extract 'formatError' tests into separate file + improve coverage

---
+++ src/error/__tests__/formatError-test.js
@@ -0,0 +1,60 @@
+// @flow strict
+
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import { formatError } from '../formatError';
+import { GraphQLError } from '../GraphQLError';
+
+describe('formatError: default error formatter', () => {
+  it('uses default message', () => {
+    // $DisableFlowOnNegativeTest
+    const e = new GraphQLError();
+
+    expect(formatError(e)).to.deep.equal({
+      message: 'An unknown error occurred.',
+      path: undefined,
+      locations: undefined,
+    });
+  });
+
+  it('includes path', () => {
+    const e = new GraphQLError('msg', null, null, null, [
+      'path',
+      3,
+      'to',
+      'field',
+    ]);
+
+    expect(formatError(e)).to.deep.equal({
+      message: 'msg',
+      locations: undefined,
+      path: ['path', 3, 'to', 'field'],
+    });
+  });
+
+  it('includes extension fields', () => {
+    const e = new GraphQLError('msg', null, null, null, null, null, {
+      foo: 'bar',
+    });
+
+    expect(formatError(e)).to.deep.equal({
+      message: 'msg',
+      locations: undefined,
+      path: undefined,
+      extensions: { foo: 'bar' },
+    });
+  });
+
+  it('rejects null and undefined errors', () => {
+    // $DisableFlowOnNegativeTest
+    expect(() => formatError(undefined)).to.throw(
+      'Received null or undefined error.',
+    );
+
+    // $DisableFlowOnNegativeTest
+    expect(() => formatError(null)).to.throw(
+      'Received null or undefined error.',
+    );
+  });
+});
