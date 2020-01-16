de4fb1aa13d797f04113bb2100d3a5f267bda15f
Travis CI <github@fb.com>
2020-01-16 15:39:33 +0000

    Deploy d951f46af588d43b2b1007e3474433ff4eb24107 to NPM branch

--- subscription/asyncIteratorReject.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Given an error, returns an AsyncIterable which will fail with that error.
- *
- * Similar to Promise.reject(error)
- */
-export default function asyncIteratorReject(error: Error): AsyncIterator<void>;
