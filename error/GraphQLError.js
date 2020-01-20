7ba1ec9fb53e67de8174e2fbb88868604afd691f
Travis CI <github@fb.com>
2020-01-20 14:43:10 +0000

    Deploy a9bf2f52a7ff5338ffa4c6f84e2ee60f01cd7571 to NPM branch

--- error/GraphQLError.js
+++ error/GraphQLError.js
@@ -3,10 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.GraphQLError = GraphQLError;
 exports.printError = printError;
+exports.GraphQLError = void 0;
 
 var _isObjectLike = _interopRequireDefault(require("../jsutils/isObjectLike"));
+
+var _symbols = require("../polyfills/symbols");
 
 var _location = require("../language/location");
 
@@ -14,150 +16,251 @@ var _printLocation = require("../language/printLocation");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function GraphQLError( // eslint-disable-line no-redeclare
-message, nodes, source, positions, path, originalError, extensions) {
-  // Compute list of blame nodes.
-  var _nodes = Array.isArray(nodes) ? nodes.length !== 0 ? nodes : undefined : nodes ? [nodes] : undefined; // Compute locations in the source for the given nodes/positions.
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+/**
+ * A GraphQLError describes an Error found during the parse, validate, or
+ * execute phases of performing a GraphQL operation. In addition to a message
+ * and stack trace, it also includes information about the locations in a
+ * GraphQL document and/or execution result that correspond to the Error.
+ */
+var GraphQLError =
+/*#__PURE__*/
+function (_Error) {
+  _inherits(GraphQLError, _Error);
+
+  /**
+   * A message describing the Error for debugging purposes.
+   *
+   * Enumerable, and appears in the result of JSON.stringify().
+   *
+   * Note: should be treated as readonly, despite invariant usage.
+   */
+
+  /**
+   * An array of { line, column } locations within the source GraphQL document
+   * which correspond to this error.
+   *
+   * Errors during validation often contain multiple locations, for example to
+   * point out two things with the same name. Errors during execution include a
+   * single location, the field which produced the error.
+   *
+   * Enumerable, and appears in the result of JSON.stringify().
+   */
+
+  /**
+   * An array describing the JSON-path into the execution response which
+   * corresponds to this error. Only included for errors during execution.
+   *
+   * Enumerable, and appears in the result of JSON.stringify().
+   */
+
+  /**
+   * An array of GraphQL AST Nodes corresponding to this error.
+   */
+
+  /**
+   * The source GraphQL document for the first location of this error.
+   *
+   * Note that if this Error represents more than one node, the source may not
+   * represent nodes after the first node.
+   */
+
+  /**
+   * An array of character offsets within the source GraphQL document
+   * which correspond to this error.
+   */
+
+  /**
+   * The original error thrown from a field resolver during execution.
+   */
+
+  /**
+   * Extension fields to add to the formatted error.
+   */
+  function GraphQLError(message, nodes, source, positions, path, originalError, extensions) {
+    var _this;
+
+    _classCallCheck(this, GraphQLError);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(GraphQLError).call(this, message)); // Compute list of blame nodes.
+
+    var _nodes = Array.isArray(nodes) ? nodes.length !== 0 ? nodes : undefined : nodes ? [nodes] : undefined; // Compute locations in the source for the given nodes/positions.
 
 
-  var _source = source;
+    var _source = source;
 
-  if (!_source && _nodes) {
-    var node = _nodes[0];
-    _source = node && node.loc && node.loc.source;
-  }
-
-  var _positions = positions;
-
-  if (!_positions && _nodes) {
-    _positions = _nodes.reduce(function (list, node) {
-      if (node.loc) {
-        list.push(node.loc.start);
-      }
-
-      return list;
-    }, []);
-  }
-
-  if (_positions && _positions.length === 0) {
-    _positions = undefined;
-  }
-
-  var _locations;
-
-  if (positions && source) {
-    _locations = positions.map(function (pos) {
-      return (0, _location.getLocation)(source, pos);
-    });
-  } else if (_nodes) {
-    _locations = _nodes.reduce(function (list, node) {
-      if (node.loc) {
-        list.push((0, _location.getLocation)(node.loc.source, node.loc.start));
-      }
-
-      return list;
-    }, []);
-  }
-
-  var _extensions = extensions;
-
-  if (_extensions == null && originalError != null) {
-    var originalExtensions = originalError.extensions;
-
-    if ((0, _isObjectLike.default)(originalExtensions)) {
-      _extensions = originalExtensions;
+    if (!_source && _nodes) {
+      var node = _nodes[0];
+      _source = node && node.loc && node.loc.source;
     }
-  }
 
-  Object.defineProperties(this, {
-    message: {
-      value: message,
-      // By being enumerable, JSON.stringify will include `message` in the
-      // resulting output. This ensures that the simplest possible GraphQL
-      // service adheres to the spec.
-      enumerable: true,
-      writable: true
-    },
-    locations: {
-      // Coercing falsy values to undefined ensures they will not be included
-      // in JSON.stringify() when not provided.
-      value: _locations || undefined,
-      // By being enumerable, JSON.stringify will include `locations` in the
-      // resulting output. This ensures that the simplest possible GraphQL
-      // service adheres to the spec.
-      enumerable: _locations != null
-    },
-    path: {
-      // Coercing falsy values to undefined ensures they will not be included
-      // in JSON.stringify() when not provided.
-      value: path || undefined,
-      // By being enumerable, JSON.stringify will include `path` in the
-      // resulting output. This ensures that the simplest possible GraphQL
-      // service adheres to the spec.
-      enumerable: path != null
-    },
-    nodes: {
-      value: _nodes || undefined
-    },
-    source: {
-      value: _source || undefined
-    },
-    positions: {
-      value: _positions || undefined
-    },
-    originalError: {
-      value: originalError
-    },
-    extensions: {
-      // Coercing falsy values to undefined ensures they will not be included
-      // in JSON.stringify() when not provided.
-      value: _extensions || undefined,
-      // By being enumerable, JSON.stringify will include `path` in the
-      // resulting output. This ensures that the simplest possible GraphQL
-      // service adheres to the spec.
-      enumerable: _extensions != null
+    var _positions = positions;
+
+    if (!_positions && _nodes) {
+      _positions = _nodes.reduce(function (list, node) {
+        if (node.loc) {
+          list.push(node.loc.start);
+        }
+
+        return list;
+      }, []);
     }
-  }); // Include (non-enumerable) stack trace.
 
-  if (originalError && originalError.stack) {
-    Object.defineProperty(this, 'stack', {
-      value: originalError.stack,
-      writable: true,
-      configurable: true
-    });
-    return;
+    if (_positions && _positions.length === 0) {
+      _positions = undefined;
+    }
+
+    var _locations;
+
+    if (positions && source) {
+      _locations = positions.map(function (pos) {
+        return (0, _location.getLocation)(source, pos);
+      });
+    } else if (_nodes) {
+      _locations = _nodes.reduce(function (list, node) {
+        if (node.loc) {
+          list.push((0, _location.getLocation)(node.loc.source, node.loc.start));
+        }
+
+        return list;
+      }, []);
+    }
+
+    var _extensions = extensions;
+
+    if (_extensions == null && originalError != null) {
+      var originalExtensions = originalError.extensions;
+
+      if ((0, _isObjectLike.default)(originalExtensions)) {
+        _extensions = originalExtensions;
+      }
+    }
+
+    Object.defineProperties(_assertThisInitialized(_this), {
+      name: {
+        value: 'GraphQLError'
+      },
+      message: {
+        value: message,
+        // By being enumerable, JSON.stringify will include `message` in the
+        // resulting output. This ensures that the simplest possible GraphQL
+        // service adheres to the spec.
+        enumerable: true,
+        writable: true
+      },
+      locations: {
+        // Coercing falsy values to undefined ensures they will not be included
+        // in JSON.stringify() when not provided.
+        value: _locations || undefined,
+        // By being enumerable, JSON.stringify will include `locations` in the
+        // resulting output. This ensures that the simplest possible GraphQL
+        // service adheres to the spec.
+        enumerable: _locations != null
+      },
+      path: {
+        // Coercing falsy values to undefined ensures they will not be included
+        // in JSON.stringify() when not provided.
+        value: path || undefined,
+        // By being enumerable, JSON.stringify will include `path` in the
+        // resulting output. This ensures that the simplest possible GraphQL
+        // service adheres to the spec.
+        enumerable: path != null
+      },
+      nodes: {
+        value: _nodes || undefined
+      },
+      source: {
+        value: _source || undefined
+      },
+      positions: {
+        value: _positions || undefined
+      },
+      originalError: {
+        value: originalError
+      },
+      extensions: {
+        // Coercing falsy values to undefined ensures they will not be included
+        // in JSON.stringify() when not provided.
+        value: _extensions || undefined,
+        // By being enumerable, JSON.stringify will include `path` in the
+        // resulting output. This ensures that the simplest possible GraphQL
+        // service adheres to the spec.
+        enumerable: _extensions != null
+      }
+    }); // Include (non-enumerable) stack trace.
+
+    if (originalError && originalError.stack) {
+      Object.defineProperty(_assertThisInitialized(_this), 'stack', {
+        value: originalError.stack,
+        writable: true,
+        configurable: true
+      });
+      return _possibleConstructorReturn(_this);
+    }
+    /* istanbul ignore next (See: https://github.com/graphql/graphql-js/issues/2317) */
+
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(_assertThisInitialized(_this), GraphQLError);
+    } else {
+      Object.defineProperty(_assertThisInitialized(_this), 'stack', {
+        value: Error().stack,
+        writable: true,
+        configurable: true
+      });
+    }
+
+    return _this;
   }
-  /* istanbul ignore next (See: https://github.com/graphql/graphql-js/issues/2317) */
 
-
-  if (Error.captureStackTrace) {
-    Error.captureStackTrace(this, GraphQLError);
-  } else {
-    Object.defineProperty(this, 'stack', {
-      value: Error().stack,
-      writable: true,
-      configurable: true
-    });
-  }
-}
-
-GraphQLError.prototype = Object.create(Error.prototype, {
-  constructor: {
-    value: GraphQLError
-  },
-  name: {
-    value: 'GraphQLError'
-  },
-  toString: {
+  _createClass(GraphQLError, [{
+    key: "toString",
     value: function toString() {
       return printError(this);
+    } // FIXME: workaround to not break chai comparisons, should be remove in v16
+    // $FlowFixMe Flow doesn't support computed properties yet
+
+  }, {
+    key: _symbols.SYMBOL_TO_STRING_TAG,
+    get: function get() {
+      return 'Object';
     }
-  }
-});
+  }]);
+
+  return GraphQLError;
+}(_wrapNativeSuper(Error));
 /**
  * Prints a GraphQLError to a string, representing useful location information
  * about the error's position in the source.
  */
+
+
+exports.GraphQLError = GraphQLError;
 
 function printError(error) {
   var output = error.message;
