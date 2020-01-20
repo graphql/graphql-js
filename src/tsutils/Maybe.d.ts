f0c717c0423cefea6e4d7efab4b60a924b56d441
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-01-20 23:46:01 +0800

    ESLint: enable more rules for TS code (#2375)

--- src/tsutils/Maybe.d.ts
+++ src/tsutils/Maybe.d.ts
@@ -1,4 +1,6 @@
 // Conveniently represents flow's "Maybe" type https://flow.org/en/docs/types/maybe/
 type Maybe<T> = null | undefined | T;
 
+// See https://github.com/typescript-eslint/typescript-eslint/issues/131
+// eslint-disable-next-line no-undef
 export default Maybe;
