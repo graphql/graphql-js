e4a18d8b94d8d014f3642a0588c0dad626f2787f
Alexander Knyazev <alexander.knyazev@progforce.com>
2020-01-22 12:04:33 +0000

    Add tests for float and int serialization to work with objects

--- src/type/__tests__/serialization-test.js
+++ src/type/__tests__/serialization-test.js
@@ -20,6 +20,14 @@ describe('Type System: Scalar coercion', () => {
     expect(GraphQLInt.serialize(1e5)).to.equal(100000);
     expect(GraphQLInt.serialize(false)).to.equal(0);
     expect(GraphQLInt.serialize(true)).to.equal(1);
+    expect(
+      GraphQLFloat.serialize({
+        value: 5,
+        valueOf() {
+          return this.value;
+        },
+      }),
+    ).to.equal(5);
 
     // The GraphQL specification does not allow serializing non-integer values
     // as Int to avoid accidental data loss.
@@ -79,6 +87,14 @@ describe('Type System: Scalar coercion', () => {
     expect(GraphQLFloat.serialize('-1.1')).to.equal(-1.1);
     expect(GraphQLFloat.serialize(false)).to.equal(0.0);
     expect(GraphQLFloat.serialize(true)).to.equal(1.0);
+    expect(
+      GraphQLFloat.serialize({
+        value: 5.5,
+        valueOf() {
+          return this.value;
+        },
+      }),
+    ).to.equal(5.5);
 
     expect(() => GraphQLFloat.serialize(NaN)).to.throw(
       'Float cannot represent non numeric value: NaN',
