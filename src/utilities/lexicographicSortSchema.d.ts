0bcef098073f1a41bc8c87fa482a05d463db52fc
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-01-29 17:42:43 +0800

    Improve description for lexicographicSortSchema (#2412)
    Replicates https://github.com/graphql-python/graphql-core-next/commit/c61c65d264044504accaa6d76b21479a2af68205

--- src/utilities/lexicographicSortSchema.d.ts
+++ src/utilities/lexicographicSortSchema.d.ts
@@ -2,5 +2,7 @@ import { GraphQLSchema } from '../type/schema';
 
 /**
  * Sort GraphQLSchema.
+ *
+ * This function returns a sorted copy of the given GraphQLSchema.
  */
 export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema;
