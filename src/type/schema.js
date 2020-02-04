aaf00b10b3408bf90775f38d07dbc29d95867fc1
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-02-04 17:19:37 +0800

    Change 'toConfig' to always return 'extensionASTNodes' as array

--- src/type/schema.js
+++ src/type/schema.js
@@ -345,7 +345,7 @@ export class GraphQLSchema {
       directives: this.getDirectives().slice(),
       extensions: this.extensions,
       astNode: this.astNode,
-      extensionASTNodes: this.extensionASTNodes,
+      extensionASTNodes: this.extensionASTNodes ?? [],
       assumeValid: this.__validationErrors !== undefined,
     };
   }
@@ -391,7 +391,7 @@ export type GraphQLSchemaNormalizedConfig = {|
   types: Array<GraphQLNamedType>,
   directives: Array<GraphQLDirective>,
   extensions: ?ReadOnlyObjMap<mixed>,
-  extensionASTNodes: ?$ReadOnlyArray<SchemaExtensionNode>,
+  extensionASTNodes: $ReadOnlyArray<SchemaExtensionNode>,
   assumeValid: boolean,
 |};
 
