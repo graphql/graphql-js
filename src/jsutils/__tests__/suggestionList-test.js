48a4282d7eff62bb7a6a08431b4b18cd50fa34c7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-02-14 16:33:18 +0530

    suggestionList: improve test coverage

--- src/jsutils/__tests__/suggestionList-test.js
+++ src/jsutils/__tests__/suggestionList-test.js
@@ -14,6 +14,34 @@ describe('suggestionList', () => {
     expect(suggestionList('input', [])).to.deep.equal([]);
   });
 
+  it('Returns options with small lexical distance', () => {
+    expect(suggestionList('greenish', ['green'])).to.deep.equal(['green']);
+    expect(suggestionList('green', ['greenish'])).to.deep.equal(['greenish']);
+  });
+
+  it('Returns options with different case', () => {
+    // cSpell:ignore verylongstring
+    expect(suggestionList('verylongstring', ['VERYLONGSTRING'])).to.deep.equal([
+      'VERYLONGSTRING',
+    ]);
+
+    expect(suggestionList('VERYLONGSTRING', ['verylongstring'])).to.deep.equal([
+      'verylongstring',
+    ]);
+
+    expect(suggestionList('VERYLONGSTRING', ['VeryLongString'])).to.deep.equal([
+      'VeryLongString',
+    ]);
+  });
+
+  it('Returns options with transpositions', () => {
+    expect(suggestionList('agr', ['arg'])).to.deep.equal(['arg']);
+
+    expect(suggestionList('214365879', ['123456789'])).to.deep.equal([
+      '123456789',
+    ]);
+  });
+
   it('Returns options sorted based on lexical distance', () => {
     expect(suggestionList('abc', ['a', 'ab', 'abc'])).to.deep.equal([
       'abc',
