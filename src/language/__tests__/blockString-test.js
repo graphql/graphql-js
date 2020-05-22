14b054d21fe09c0f8c1161d77056ebb87bf776f9
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-05-22 12:57:55 +0300

    blockString-test: add fuzzing test for 'printBlockString'
    Motivation #2512

--- src/language/__tests__/blockString-test.js
+++ src/language/__tests__/blockString-test.js
@@ -3,6 +3,14 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 
+import dedent from '../../__testUtils__/dedent';
+import inspectStr from '../../__testUtils__/inspectStr';
+import genFuzzStrings from '../../__testUtils__/genFuzzStrings';
+
+import invariant from '../../jsutils/invariant';
+
+import { Lexer } from '../lexer';
+import { Source } from '../source';
 import {
   dedentBlockStringValue,
   getBlockStringIndentation,
@@ -180,5 +188,58 @@ describe('printBlockString', () => {
         '"""',
       ),
     );
+  });
+
+  it('correctly print random strings', () => {
+    // Testing with length >5 is taking exponentially more time. However it is
+    // highly recommended to test with increased limit if you make any change.
+    for (const fuzzStr of genFuzzStrings({
+      allowedChars: ['\n', '\t', ' ', '"', 'a', '\\'],
+      maxLength: 5,
+    })) {
+      const testStr = '"""' + fuzzStr + '"""';
+
+      let testValue;
+      try {
+        testValue = lexValue(testStr);
+      } catch (e) {
+        continue; // skip invalid values
+      }
+      invariant(typeof testValue === 'string');
+
+      const printedValue = lexValue(printBlockString(testValue));
+
+      invariant(
+        testValue === printedValue,
+        dedent`
+          Expected lexValue(printBlockString(${inspectStr(testValue)}))
+            to equal ${inspectStr(testValue)}
+            but got  ${inspectStr(printedValue)}
+        `,
+      );
+
+      const printedMultilineString = lexValue(
+        printBlockString(testValue, ' ', true),
+      );
+
+      invariant(
+        testValue === printedMultilineString,
+        dedent`
+          Expected lexValue(printBlockString(${inspectStr(
+            testValue,
+          )}, ' ', true))
+            to equal ${inspectStr(testValue)}
+            but got  ${inspectStr(printedMultilineString)}
+        `,
+      );
+    }
+
+    function lexValue(str) {
+      const lexer = new Lexer(new Source(str));
+      const value = lexer.advance().value;
+
+      invariant(lexer.advance().kind === '<EOF>', 'Expected EOF');
+      return value;
+    }
   });
 });
