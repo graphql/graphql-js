90cc94e87e0ce3a673b4097112c39eed86ff9cf6
Dotan Simha <dotansimha@gmail.com>
2020-06-07 09:14:56 +0300

    Update src/__fixtures__/index.d.ts
    Co-authored-by: Ivan Goncharov <ivan.goncharov.ua@gmail.com>

--- src/__fixtures__/index.d.ts
+++ src/__fixtures__/index.d.ts
@@ -1,4 +1,4 @@
 export declare const bigSchemaSDL: string;
-export declare const bigSchemaIntrospectionResult: Record<string, any>;
+export declare const bigSchemaIntrospectionResult: { data: unknown };
 export declare const kitchenSinkSDL: string;
 export declare const kitchenSinkQuery: string;
