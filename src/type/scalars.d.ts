92567d65dd7b2c230d3221883196000ff6a89142
overhead525 <overheadexistence@gmail.com>
2020-06-07 19:43:51 -0400

    fix types for flow

--- src/type/scalars.d.ts
+++ src/type/scalars.d.ts
@@ -6,6 +6,9 @@ export const GraphQLString: GraphQLScalarType;
 export const GraphQLBoolean: GraphQLScalarType;
 export const GraphQLID: GraphQLScalarType;
 
+export declare const MAX_INT: number;
+export declare const MIN_INT: number;
+
 export const specifiedScalarTypes: ReadonlyArray<GraphQLScalarType>;
 
 export function isSpecifiedScalarType(type: GraphQLNamedType): boolean;
