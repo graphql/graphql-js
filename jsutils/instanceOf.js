610fa176fccfd83746b11b87694c4a1d06a89ba9
GitHub Action Script <please@open.issue>
2020-06-14 12:01:14 +0000

    Deploy 2736f457b716b250c446b072c910f97e19943568 to 'deno' branch

--- jsutils/instanceOf.js
+++ jsutils/instanceOf.js
@@ -4,7 +4,7 @@
  */
 // See: https://expressjs.com/en/advanced/best-practice-performance.html#set-node_env-to-production
 // See: https://webpack.js.org/guides/production/
-export default isProductionEnvironment() ? // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
+export default process.env.NODE_ENV === 'production' ? // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
 // eslint-disable-next-line no-shadow
 function instanceOf(value, constructor) {
   return value instanceof constructor;
@@ -36,26 +36,3 @@ spurious results.`);
 
   return false;
 };
-/* global Deno */
-
-/* :: declare var Deno: any; */
-
-function isProductionEnvironment() {
-  // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
-  try {
-    if (process.env.NODE_ENV === 'production') {
-      return true;
-    }
-  } catch (e) {} // ignore
-  // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
-
-
-  try {
-    if (Deno.env.get('DENO_ENV') === 'production') {
-      return true;
-    }
-  } catch (e) {// ignore
-  }
-
-  return false;
-}
