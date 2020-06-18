0e97527aa0a4b438ae351f0ba24bd65388e6f499
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-06-18 17:16:34 +0300

    Run cspell on entire project (#2665)

--- resources/checkgit.sh
+++ resources/checkgit.sh
@@ -13,18 +13,18 @@ git fetch;
 if [[ $? -ne 0 ]]; then exit 1; fi;
 
 # Extract useful information.
-GITBRANCH=$(git branch -v 2> /dev/null | sed '/^[^*]/d');
-GITBRANCHNAME=$(echo "$GITBRANCH" | sed 's/* \([A-Za-z0-9_\-]*\).*/\1/');
-GITBRANCHSYNC=$(echo "$GITBRANCH" | sed 's/* [^[]*.\([^]]*\).*/\1/');
+GIT_BRANCH=$(git branch -v 2> /dev/null | sed '/^[^*]/d');
+GIT_BRANCH_NAME=$(echo "$GIT_BRANCH" | sed 's/* \([A-Za-z0-9_\-]*\).*/\1/');
+GIT_BRANCH_SYNC=$(echo "$GIT_BRANCH" | sed 's/* [^[]*.\([^]]*\).*/\1/');
 
 # Check if master is checked out
-if [ "$GITBRANCHNAME" != "master" ]; then
-  read -p "Git not on master but $GITBRANCHNAME. Continue? (y|N) " yn;
+if [ "$GIT_BRANCH_NAME" != "master" ]; then
+  read -p "Git not on master but $GIT_BRANCH_NAME. Continue? (y|N) " yn;
   if [ "$yn" != "y" ]; then exit 1; fi;
 fi;
 
 # Check if branch is synced with remote
-if [ "$GITBRANCHSYNC" != "" ]; then
-  read -p "Git not up to date but $GITBRANCHSYNC. Continue? (y|N) " yn;
+if [ "$GIT_BRANCH_SYNC" != "" ]; then
+  read -p "Git not up to date but $GIT_BRANCH_SYNC. Continue? (y|N) " yn;
   if [ "$yn" != "y" ]; then exit 1; fi;
 fi;
