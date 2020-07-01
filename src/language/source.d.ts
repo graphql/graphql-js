38b4f9ebe0076325277689b851d92d4585cb49e1
Daniel Rearden <danielrearden@gmail.com>
2020-07-01 09:00:27 -0400

    Sync JSDoc comments with docs changes (#2681)

--- src/language/source.d.ts
+++ src/language/source.d.ts
@@ -4,12 +4,11 @@ interface Location {
 }
 
 /**
- * A representation of source input to GraphQL.
- * `name` and `locationOffset` are optional. They are useful for clients who
- * store GraphQL documents in source files; for example, if the GraphQL input
- * starts at line 40 in a file named Foo.graphql, it might be useful for name to
- * be "Foo.graphql" and location to be `{ line: 40, column: 0 }`.
- * line and column in locationOffset are 1-indexed
+ * A representation of source input to GraphQL. The `name` and `locationOffset` parameters are
+ * optional, but they are useful for clients who store GraphQL documents in source files.
+ * For example, if the GraphQL input starts at line 40 in a file named `Foo.graphql`, it might
+ * be useful for `name` to be `"Foo.graphql"` and location to be `{ line: 40, column: 1 }`.
+ * The `line` and `column` properties in `locationOffset` are 1-indexed.
  */
 export class Source {
   body: string;
