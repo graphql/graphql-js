1ef0d5849f4f5b2c3ce0741fab4a7a856e7c0d7a
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-08-26 12:56:50 +0300

    Remove deprecated rule aliases

--- src/validation/rules/LoneSchemaDefinition.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated and will be removed in v16
- * Please use either:
- *   import { LoneSchemaDefinitionRule } from 'graphql'
- * or
- *   import { LoneSchemaDefinitionRule } from 'graphql/validation'
- */
-export { LoneSchemaDefinitionRule as LoneSchemaDefinition } from './LoneSchemaDefinitionRule';
