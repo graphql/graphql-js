f3e2954126e0005f7b01b2954071d4a91fd4f2b2
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-08-31 16:48:30 +0300

    Move 'isAsyncIterable' into 'jsutils' (#2775)

---
+++ src/jsutils/isAsyncIterable.js
@@ -0,0 +1,17 @@
+import { SYMBOL_ASYNC_ITERATOR } from '../polyfills/symbols';
+
+/**
+ * Returns true if the provided object implements the AsyncIterator protocol via
+ * either implementing a `Symbol.asyncIterator` or `"@@asyncIterator"` method.
+ */
+declare function isAsyncIterable(value: mixed): boolean %checks(value instanceof
+  AsyncIterable);
+
+// eslint-disable-next-line no-redeclare
+export default function isAsyncIterable(maybeAsyncIterable) {
+  if (maybeAsyncIterable == null || typeof maybeAsyncIterable !== 'object') {
+    return false;
+  }
+
+  return typeof maybeAsyncIterable[SYMBOL_ASYNC_ITERATOR] === 'function';
+}
