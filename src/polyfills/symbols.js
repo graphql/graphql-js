abcc4b010f6fc733036e775fc52bde32457ac22c
dionisnote <dionisnote@gmail.com>
2020-09-18 19:46:54 +0400

    fix: Added check for specific symbols in polyfills/symbols

--- src/polyfills/symbols.js
+++ src/polyfills/symbols.js
@@ -1,13 +1,19 @@
 // In ES2015 (or a polyfilled) environment, this will be Symbol.iterator
 // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
 export const SYMBOL_ITERATOR: string =
-  typeof Symbol === 'function' ? Symbol.iterator : '@@iterator';
+  typeof Symbol === 'function' && Boolean(Symbol.iterator)
+    ? Symbol.iterator
+    : '@@iterator';
 
 // In ES2017 (or a polyfilled) environment, this will be Symbol.asyncIterator
 // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
 export const SYMBOL_ASYNC_ITERATOR: string =
-  typeof Symbol === 'function' ? Symbol.asyncIterator : '@@asyncIterator';
+  typeof Symbol === 'function' && Boolean(Symbol.asyncIterator)
+    ? Symbol.asyncIterator
+    : '@@asyncIterator';
 
 // istanbul ignore next (See: 'https://github.com/graphql/graphql-js/issues/2317')
 export const SYMBOL_TO_STRING_TAG: string =
-  typeof Symbol === 'function' ? Symbol.toStringTag : '@@toStringTag';
+  typeof Symbol === 'function' && Boolean(Symbol.toStringTag)
+    ? Symbol.toStringTag
+    : '@@toStringTag';
