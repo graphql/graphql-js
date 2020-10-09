711425eaf3baa3efa5da10e11ed76f58ec149ad7
Forbes Lindesay <forbes@lindesay.co.uk>
2020-10-09 21:59:31 +0100

    fix: ensure variance of types matches how values are used (#2786)
    Co-authored-by: Ivan Goncharov <ivan.goncharov.ua@gmail.com>
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>
    Co-authored-by: Arda Tanrıkulu <20847995+ardatan@users.noreply.github.com>

--- src/utilities/typedQueryDocumentNode.d.ts
+++ src/utilities/typedQueryDocumentNode.d.ts
@@ -9,6 +9,12 @@ export interface TypedQueryDocumentNode<
 > extends DocumentNode {
   readonly definitions: ReadonlyArray<ExecutableDefinitionNode>;
   // FIXME: remove once TS implements proper way to enforce nominal typing
-  readonly __enforceStructuralTypingOnResponseDataType?: TResponseData;
-  readonly __enforceStructuralTypingOnRequestVariablesType?: TRequestVariables;
+  /**
+   * This type is used to ensure that the variables you pass in to the query are assignable to Variables
+   * and that the Result is assignable to whatever you pass your result to. The method is never actually
+   * implemented, but the type is valid because we list it as optional
+   */
+  __ensureTypesOfVariablesAndResultMatching?: (
+    variables: TRequestVariables,
+  ) => TResponseData;
 }
