b62fe3d94e9404875b32ad24f5af18408a06d041
saihaj <saihajpreet.singh@gmail.com>
2020-10-30 16:54:09 -0500

    make memoize arguments type weakmap

--- src/jsutils/memoize3.ts
+++ src/jsutils/memoize3.ts
@@ -2,10 +2,10 @@
  * Memoizes the provided three-argument function.
  */
 export function memoize3<
-  A1 extends {} | ReadonlyArray<unknown>,
-  A2 extends {} | ReadonlyArray<unknown>,
-  A3 extends {} | ReadonlyArray<unknown>,
-  R, 
+  A1 extends object,
+  A2 extends object,
+  A3 extends object,
+  R
 >(fn: (a1: A1, a2: A2, a3: A3) => R): (a1: A1, a2: A2, a3: A3) => R {
   let cache0;
 
