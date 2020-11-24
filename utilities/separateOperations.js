0cdacbfd8b860d0dcbb5391e46c84383a58d0a34
GitHub Action Script <please@open.issue>
2020-11-24 22:28:19 +0000

    Deploy cd273ad136d615b3f2f4c830bd8891c7c5590c30 to 'deno' branch

--- utilities/separateOperations.js
+++ utilities/separateOperations.js
@@ -9,66 +9,66 @@ import { visit } from "../language/visitor.js";
 
 export function separateOperations(documentAST) {
   const operations = [];
-  const depGraph = Object.create(null);
-  let fromName; // Populate metadata and build a dependency graph.
+  const depGraph = Object.create(null); // Populate metadata and build a dependency graph.
 
-  visit(documentAST, {
-    OperationDefinition(node) {
-      fromName = opName(node);
-      operations.push(node);
-    },
+  for (const definitionNode of documentAST.definitions) {
+    switch (definitionNode.kind) {
+      case Kind.OPERATION_DEFINITION:
+        operations.push(definitionNode);
+        break;
 
-    FragmentDefinition(node) {
-      fromName = node.name.value;
-    },
-
-    FragmentSpread(node) {
-      const toName = node.name.value;
-      let dependents = depGraph[fromName];
-
-      if (dependents === undefined) {
-        dependents = depGraph[fromName] = Object.create(null);
-      }
-
-      dependents[toName] = true;
+      case Kind.FRAGMENT_DEFINITION:
+        depGraph[definitionNode.name.value] = collectDependencies(definitionNode.selectionSet);
+        break;
     }
-
-  }); // For each operation, produce a new synthesized AST which includes only what
+  } // For each operation, produce a new synthesized AST which includes only what
   // is necessary for completing that operation.
+
 
   const separatedDocumentASTs = Object.create(null);
 
   for (const operation of operations) {
-    const operationName = opName(operation);
-    const dependencies = Object.create(null);
-    collectTransitiveDependencies(dependencies, depGraph, operationName); // The list of definition nodes to be included for this operation, sorted
+    const dependencies = new Set();
+
+    for (const fragmentName of collectDependencies(operation.selectionSet)) {
+      collectTransitiveDependencies(dependencies, depGraph, fragmentName);
+    } // Provides the empty string for anonymous operations.
+
+
+    const operationName = operation.name ? operation.name.value : ''; // The list of definition nodes to be included for this operation, sorted
     // to retain the same order as the original document.
 
     separatedDocumentASTs[operationName] = {
       kind: Kind.DOCUMENT,
-      definitions: documentAST.definitions.filter(node => node === operation || node.kind === Kind.FRAGMENT_DEFINITION && dependencies[node.name.value])
+      definitions: documentAST.definitions.filter(node => node === operation || node.kind === Kind.FRAGMENT_DEFINITION && dependencies.has(node.name.value))
     };
   }
 
   return separatedDocumentASTs;
 }
 
-// Provides the empty string for anonymous operations.
-function opName(operation) {
-  return operation.name ? operation.name.value : '';
-} // From a dependency graph, collects a list of transitive dependencies by
+// From a dependency graph, collects a list of transitive dependencies by
 // recursing through a dependency graph.
-
-
 function collectTransitiveDependencies(collected, depGraph, fromName) {
-  const immediateDeps = depGraph[fromName];
+  if (!collected.has(fromName)) {
+    collected.add(fromName);
+    const immediateDeps = depGraph[fromName];
 
-  if (immediateDeps) {
-    for (const toName of Object.keys(immediateDeps)) {
-      if (!collected[toName]) {
-        collected[toName] = true;
+    if (immediateDeps !== undefined) {
+      for (const toName of immediateDeps) {
         collectTransitiveDependencies(collected, depGraph, toName);
       }
     }
   }
+}
+
+function collectDependencies(selectionSet) {
+  const dependencies = [];
+  visit(selectionSet, {
+    FragmentSpread(node) {
+      dependencies.push(node.name.value);
+    }
+
+  });
+  return dependencies;
 }
