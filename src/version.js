1611bbb08a88f734e9490b14cfe6afea11a838e0
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2021-01-26 18:38:02 +0200

    15.5.0

--- src/version.js
+++ src/version.js
@@ -6,14 +6,14 @@
 /**
  * A string containing the version of the GraphQL.js library
  */
-export const version = '15.4.0';
+export const version = '15.5.0';
 
 /**
  * An object containing the components of the GraphQL.js version string
  */
 export const versionInfo = Object.freeze({
   major: 15,
-  minor: 4,
+  minor: 5,
   patch: 0,
   preReleaseTag: null,
 });
