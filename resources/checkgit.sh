a546aca77922beb2fee949ea0ad7c9234f7006fd
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-01-27 04:50:39 -0600

    ci: migrate master to main (#2881)

--- resources/checkgit.sh
+++ resources/checkgit.sh
@@ -2,10 +2,10 @@
 trap "exit 1" ERR
 
 #
-# This script determines if current git state is the up to date master. If so
+# This script determines if current git state is the up to date main. If so
 # it exits normally. If not it prompts for an explicit continue. This script
 # intends to protect from versioning for NPM without first pushing changes
-# and including any changes on master.
+# and including any changes on main.
 #
 
 # First fetch to ensure git is up to date. Fail-fast if this fails.
@@ -17,9 +17,9 @@ GIT_BRANCH=$(git branch -v 2> /dev/null | sed '/^[^*]/d');
 GIT_BRANCH_NAME=$(echo "$GIT_BRANCH" | sed 's/* \([A-Za-z0-9_\-]*\).*/\1/');
 GIT_BRANCH_SYNC=$(echo "$GIT_BRANCH" | sed 's/* [^[]*.\([^]]*\).*/\1/');
 
-# Check if master is checked out
-if [ "$GIT_BRANCH_NAME" != "master" ]; then
-  read -p "Git not on master but $GIT_BRANCH_NAME. Continue? (y|N) " yn;
+# Check if main is checked out
+if [ "$GIT_BRANCH_NAME" != "main" ]; then
+  read -p "Git not on main but $GIT_BRANCH_NAME. Continue? (y|N) " yn;
   if [ "$yn" != "y" ]; then exit 1; fi;
 fi;
 
