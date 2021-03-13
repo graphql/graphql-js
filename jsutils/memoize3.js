176262699a0f0a6b66caf1c114d3ad0d6c792a06
GitHub Action Script <please@open.issue>
2021-03-13 17:57:10 +0000

    Deploy 26e4568df9d42d5f5a2568f5071fa7dd1bb755e2 to 'npm' branch

--- jsutils/memoize3.js
+++ jsutils/memoize3.js
@@ -11,35 +11,31 @@ exports.memoize3 = memoize3;
 function memoize3(fn) {
   let cache0;
   return function memoized(a1, a2, a3) {
-    if (!cache0) {
+    if (cache0 === undefined) {
       cache0 = new WeakMap();
     }
 
     let cache1 = cache0.get(a1);
-    let cache2;
 
-    if (cache1) {
-      cache2 = cache1.get(a2);
-
-      if (cache2) {
-        const cachedValue = cache2.get(a3);
-
-        if (cachedValue !== undefined) {
-          return cachedValue;
-        }
-      }
-    } else {
+    if (cache1 === undefined) {
       cache1 = new WeakMap();
       cache0.set(a1, cache1);
     }
 
-    if (!cache2) {
+    let cache2 = cache1.get(a2);
+
+    if (cache2 === undefined) {
       cache2 = new WeakMap();
       cache1.set(a2, cache2);
     }
 
-    const newValue = fn(a1, a2, a3);
-    cache2.set(a3, newValue);
-    return newValue;
+    let fnResult = cache2.get(a3);
+
+    if (fnResult === undefined) {
+      fnResult = fn(a1, a2, a3);
+      cache2.set(a3, fnResult);
+    }
+
+    return fnResult;
   };
 }
