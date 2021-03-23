754c1fb5fc20c5fce23d0c9a7c82822e46d26bbe
GitHub Action Script <please@open.issue>
2021-03-23 17:42:08 +0000

    Deploy a50dbd24e7ee4ca60e4405bb5794385460ddb7db to 'npm' branch

--- language/printLocation.mjs
+++ language/printLocation.mjs
@@ -12,7 +12,7 @@ export function printLocation(location) {
 
 export function printSourceLocation(source, sourceLocation) {
   const firstLineColumnOffset = source.locationOffset.column - 1;
-  const body = whitespace(firstLineColumnOffset) + source.body;
+  const body = ''.padStart(firstLineColumnOffset) + source.body;
   const lineIndex = sourceLocation.line - 1;
   const lineOffset = source.locationOffset.line - 1;
   const lineNum = sourceLocation.line + lineOffset;
@@ -31,23 +31,15 @@ export function printSourceLocation(source, sourceLocation) {
       subLines.push(locationLine.slice(i, i + 80));
     }
 
-    return locationStr + printPrefixedLines([[`${lineNum}`, subLines[0]], ...subLines.slice(1, subLineIndex + 1).map(subLine => ['', subLine]), [' ', whitespace(subLineColumnNum - 1) + '^'], ['', subLines[subLineIndex + 1]]]);
+    return locationStr + printPrefixedLines([[`${lineNum} |`, subLines[0]], ...subLines.slice(1, subLineIndex + 1).map(subLine => ['|', subLine]), ['|', '^'.padStart(subLineColumnNum)], ['|', subLines[subLineIndex + 1]]]);
   }
 
   return locationStr + printPrefixedLines([// Lines specified like this: ["prefix", "string"],
-  [`${lineNum - 1}`, lines[lineIndex - 1]], [`${lineNum}`, locationLine], ['', whitespace(columnNum - 1) + '^'], [`${lineNum + 1}`, lines[lineIndex + 1]]]);
+  [`${lineNum - 1} |`, lines[lineIndex - 1]], [`${lineNum} |`, locationLine], ['|', '^'.padStart(columnNum)], [`${lineNum + 1} |`, lines[lineIndex + 1]]]);
 }
 
 function printPrefixedLines(lines) {
   const existingLines = lines.filter(([_, line]) => line !== undefined);
   const padLen = Math.max(...existingLines.map(([prefix]) => prefix.length));
-  return existingLines.map(([prefix, line]) => leftPad(padLen, prefix) + (line ? ' | ' + line : ' |')).join('\n');
-}
-
-function whitespace(len) {
-  return Array(len + 1).join(' ');
-}
-
-function leftPad(len, str) {
-  return whitespace(len - str.length) + str;
+  return existingLines.map(([prefix, line]) => prefix.padStart(padLen) + (line ? ' ' + line : '')).join('\n');
 }
