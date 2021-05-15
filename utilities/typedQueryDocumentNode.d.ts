069f5ee8a5f7ebe0efd72ce5a34baaf5058e0b70
GitHub Action Script <please@open.issue>
2021-05-15 16:11:31 +0000

    Deploy e94c2fa42992bd58f92709189c1ccc4b203d11b5 to 'deno' branch

--- utilities/typedQueryDocumentNode.d.ts
+++ utilities/typedQueryDocumentNode.d.ts
@@ -5,7 +5,7 @@ import { DocumentNode, ExecutableDefinitionNode } from '../language/ast';
  */
 export interface TypedQueryDocumentNode<
   TResponseData = Record<string, any>,
-  TRequestVariables = Record<string, any>
+  TRequestVariables = Record<string, any>,
 > extends DocumentNode {
   readonly definitions: ReadonlyArray<ExecutableDefinitionNode>;
   // FIXME: remove once TS implements proper way to enforce nominal typing
