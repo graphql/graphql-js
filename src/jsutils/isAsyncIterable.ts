eb32d75b8752645718244b6da1a6d1ec146e1bd1
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-05-15 10:04:48 -0400

    Switch to TS syntax (#3090)

--- src/jsutils/isAsyncIterable.ts
+++ src/jsutils/isAsyncIterable.ts
@@ -2,12 +2,8 @@
  * Returns true if the provided object implements the AsyncIterator protocol via
  * implementing a `Symbol.asyncIterator` method.
  */
-declare function isAsyncIterable(
-  value: mixed,
-  // $FlowFixMe[invalid-in-rhs]
-): boolean %checks(value instanceof AsyncIterable);
-
-// eslint-disable-next-line no-redeclare
-export function isAsyncIterable(maybeAsyncIterable) {
+export function isAsyncIterable(
+  maybeAsyncIterable: unknown,
+): maybeAsyncIterable is AsyncIterable<unknown> {
   return typeof maybeAsyncIterable?.[Symbol.asyncIterator] === 'function';
 }
