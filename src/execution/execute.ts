0a98e13dc07e9a7ea7cabb96e9e33498f5fc92bd
Young Min Kim <youngmink@netflix.com>
2021-05-18 11:56:29 -0700

    Start importing Wei's changes

--- src/execution/execute.ts
+++ src/execution/execute.ts
@@ -51,6 +51,7 @@ import {
   GraphQLSkipDirective,
 } from '../type/directives';
 import {
+  GraphQLNonNull,
   isObjectType,
   isAbstractType,
   isLeafType,
@@ -607,7 +608,9 @@ function resolveField(
     return;
   }
 
-  const returnType = fieldDef.type;
+  const returnType = fieldNodes[0].required // TODO: Need to update FieldNode
+    ? new GraphQLNonNull(fieldDef.type)
+    : fieldDef.type;
   const resolveFn = fieldDef.resolve ?? exeContext.fieldResolver;
 
   const info = buildResolveInfo(
