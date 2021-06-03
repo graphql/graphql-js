f1baef678e850ee43848eddb4bf54e1f1f23f57e
GitHub Action Script <please@open.issue>
2021-06-03 23:05:48 +0000

    Deploy fd3ab05cccaa35db1fb3fac7b989a71c26c5aa73 to 'npm' branch

--- version.js
+++ version.js
@@ -10,7 +10,7 @@ exports.versionInfo = exports.version = void 0;
 /**
  * A string containing the version of the GraphQL.js library
  */
-const version = '16.0.0-alpha.2';
+const version = '16.0.0-alpha.3';
 /**
  * An object containing the components of the GraphQL.js version string
  */
@@ -20,6 +20,6 @@ const versionInfo = Object.freeze({
   major: 16,
   minor: 0,
   patch: 0,
-  preReleaseTag: 'alpha.2',
+  preReleaseTag: 'alpha.3',
 });
 exports.versionInfo = versionInfo;
