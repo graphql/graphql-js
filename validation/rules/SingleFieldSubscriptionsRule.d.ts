47b2cff9cdb1968524023f072998503ce428909d
GitHub Action Script <please@open.issue>
2021-06-03 17:53:50 +0000

    Deploy d5eac891ccb2c0b27b5b38101448d8fc1c3d8f89 to 'npm' branch

--- validation/rules/SingleFieldSubscriptionsRule.d.ts
+++ validation/rules/SingleFieldSubscriptionsRule.d.ts
@@ -1,10 +1,11 @@
 import type { ASTVisitor } from '../../language/visitor';
-import type { ASTValidationContext } from '../ValidationContext';
+import type { ValidationContext } from '../ValidationContext';
 /**
- * Subscriptions must only include one field.
+ * Subscriptions must only include a non-introspection field.
  *
- * A GraphQL subscription is valid only if it contains a single root field.
+ * A GraphQL subscription is valid only if it contains a single root field and
+ * that root field is not an introspection field.
  */
 export declare function SingleFieldSubscriptionsRule(
-  context: ASTValidationContext,
+  context: ValidationContext,
 ): ASTVisitor;
--- validation/rules/SingleFieldSubscriptionsRule.js
+++ validation/rules/SingleFieldSubscriptionsRule.js
@@ -7,24 +7,83 @@ exports.SingleFieldSubscriptionsRule = SingleFieldSubscriptionsRule;
 
 var _GraphQLError = require('../../error/GraphQLError.js');
 
+var _kinds = require('../../language/kinds.js');
+
+var _execute = require('../../execution/execute.js');
+
 /**
- * Subscriptions must only include one field.
+ * Subscriptions must only include a non-introspection field.
  *
- * A GraphQL subscription is valid only if it contains a single root field.
+ * A GraphQL subscription is valid only if it contains a single root field and
+ * that root field is not an introspection field.
  */
 function SingleFieldSubscriptionsRule(context) {
   return {
     OperationDefinition(node) {
       if (node.operation === 'subscription') {
-        if (node.selectionSet.selections.length !== 1) {
-          context.reportError(
-            new _GraphQLError.GraphQLError(
-              node.name
-                ? `Subscription "${node.name.value}" must select only one top level field.`
-                : 'Anonymous Subscription must select only one top level field.',
-              node.selectionSet.selections.slice(1),
-            ),
+        const schema = context.getSchema();
+        const subscriptionType = schema.getSubscriptionType();
+
+        if (subscriptionType) {
+          const operationName = node.name ? node.name.value : null;
+          const variableValues = Object.create(null);
+          const document = context.getDocument();
+          const fragments = Object.create(null);
+
+          for (const definition of document.definitions) {
+            if (definition.kind === _kinds.Kind.FRAGMENT_DEFINITION) {
+              fragments[definition.name.value] = definition;
+            }
+          } // FIXME: refactor out `collectFields` into utility function that doesn't need fake context.
+
+          const fakeExecutionContext = {
+            schema,
+            fragments,
+            rootValue: undefined,
+            contextValue: undefined,
+            operation: node,
+            variableValues,
+            fieldResolver: _execute.defaultFieldResolver,
+            typeResolver: _execute.defaultTypeResolver,
+            errors: [],
+          };
+          const fields = (0, _execute.collectFields)(
+            fakeExecutionContext,
+            subscriptionType,
+            node.selectionSet,
+            new Map(),
+            new Set(),
           );
+
+          if (fields.size > 1) {
+            const fieldSelectionLists = [...fields.values()];
+            const extraFieldSelectionLists = fieldSelectionLists.slice(1);
+            const extraFieldSelections = extraFieldSelectionLists.flat();
+            context.reportError(
+              new _GraphQLError.GraphQLError(
+                operationName != null
+                  ? `Subscription "${operationName}" must select only one top level field.`
+                  : 'Anonymous Subscription must select only one top level field.',
+                extraFieldSelections,
+              ),
+            );
+          }
+
+          for (const fieldNodes of fields.values()) {
+            const field = fieldNodes[0];
+            const fieldName = field.name.value;
+
+            if (fieldName[0] === '_' && fieldName[1] === '_') {
+              context.reportError(
+                new _GraphQLError.GraphQLError(
+                  operationName != null
+                    ? `Subscription "${operationName}" must not select an introspection top level field.`
+                    : 'Anonymous Subscription must not select an introspection top level field.',
+                  fieldNodes,
+                ),
+              );
+            }
+          }
         }
       }
     },
