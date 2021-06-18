d3f99c6c6646e7a2ee4c87a41343c963518fbcd1
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-06-18 03:00:59 +0300

    fix

--- src/execution/index.ts
+++ src/execution/index.ts
@@ -1,13 +1,12 @@
 export { pathToArray as responsePathAsArray } from '../jsutils/Path';
 
-export { Executor } from './executor';
-
 export {
-  execute,
-  executeSync,
+  Executor,
   defaultFieldResolver,
   defaultTypeResolver,
-} from './execute';
+} from './executor';
+
+export { execute, executeSync } from './execute';
 
 export type {
   ExecutionArgs,
