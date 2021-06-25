6e07bb62fc755f7158039050e43f906ce3ae158f
Michael Glenn <mbg198618@gmail.com>
2021-06-24 21:56:57 -0500

    Update graphql-js-npm-install.js

--- resources/graphql-js-npm-install.js
+++ resources/graphql-js-npm-install.js
@@ -1,1 +1,21 @@
 npm install --save graphql
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+
+var schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        },
+      },
+    },
+  }),
+});
