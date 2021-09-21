c7906e2863bdd357a4ffbe065f6f74840c72392b
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2021-09-21 00:28:52 -0400

    scripts and prettier

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -14,10 +14,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/tutorials"
-          >
+          <Link className="button button--secondary button--lg" to="/tutorials">
             Get Started
           </Link>
         </div>
