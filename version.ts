cf0eaeecbb58c9027abbd680e75f65aa306e19f0
GitHub Action Script <please@open.issue>
2021-10-18 13:09:57 +0000

    Deploy 4c9bf037ed32b8a9bd2215b264c8b89e1f6c86c2 to 'deno' branch

--- version.ts
+++ version.ts
@@ -4,7 +4,7 @@
 /**
  * A string containing the version of the GraphQL.js library
  */
-export const version = '16.0.0-rc.4';
+export const version = '16.0.0-rc.5';
 /**
  * An object containing the components of the GraphQL.js version string
  */
@@ -13,5 +13,5 @@ export const versionInfo = Object.freeze({
   major: 16,
   minor: 0,
   patch: 0,
-  preReleaseTag: 'rc.4',
+  preReleaseTag: 'rc.5',
 });
