283c797d2d89be823cd82573b3c2497872246a4f
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2021-10-22 17:40:30 +0300

    Change type of error extensions from anonymous Record to named interfaces
    Backport of #3315

--- src/error/index.d.ts
+++ src/error/index.d.ts
@@ -1,4 +1,8 @@
-export { GraphQLError, printError } from './GraphQLError';
+export {
+  GraphQLError,
+  GraphQLErrorExtensions,
+  printError,
+} from './GraphQLError';
 export { syntaxError } from './syntaxError';
 export { locatedError } from './locatedError';
 export { formatError, GraphQLFormattedError } from './formatError';
