graphql/graphql-js

commit 7c6bf480b5cfc09451b2951c6303351a76785dc1

Add message that we only support TS >= 4.1.0 (#3319)

resources/build-npm.js
@@ -18,6 +18,8 @@ if (require.main === module) {
   fs.rmSync('./npmDist', { recursive: true, force: true });
   fs.mkdirSync('./npmDist');
 
+  const packageJSON = buildPackageJSON();
+
   const srcFiles = readdirRecursive('./src', { ignoreDir: /^__.*__$/ });
   for (const filepath of srcFiles) {
     const srcPath = path.join('./src', filepath);
@@ -48,11 +50,23 @@ if (require.main === module) {
     'Fail to generate `*.d.ts` files, please run `npm run check`',
   );
 
+  assert(packageJSON.types, 'Missing "types".');
+  const supportedTSVersions = Object.keys(packageJSON.typesVersions);
+  assert(
+    supportedTSVersions.length === 1,
+    'Property "typesVersions" should have exactly one key.',
+  );
+  // TODO: revisit once TS implements https://github.com/microsoft/TypeScript/issues/44795
+  fs.writeFileSync(
+    path.join('./npmDist', packageJSON.types),
+    // Provoke syntax error to show this message
+    `"Package 'graphql' support only TS versions that are ${supportedTSVersions[0]}".`,
+  );
+
   fs.copyFileSync('./LICENSE', './npmDist/LICENSE');
   fs.copyFileSync('./README.md', './npmDist/README.md');
 
   // Should be done as the last step so only valid packages can be published
-  const packageJSON = buildPackageJSON();
   writeGeneratedFile('./npmDist/package.json', JSON.stringify(packageJSON));
 
   showDirStats('./npmDist');

commit c9180df1761306058d4b6820d4f85c02cf720291

Fix TS error caused by importing internal files directly
Fixes #3338
Caused by #3319

resources/build-npm.js
@@ -50,18 +50,23 @@ if (require.main === module) {
     'Fail to generate `*.d.ts` files, please run `npm run check`',
   );
 
-  assert(packageJSON.types, 'Missing "types".');
+  assert(packageJSON.types === undefined, 'Unexpected "types" in package.json');
   const supportedTSVersions = Object.keys(packageJSON.typesVersions);
   assert(
     supportedTSVersions.length === 1,
     'Property "typesVersions" should have exactly one key.',
   );
-  // TODO: revisit once TS implements https://github.com/microsoft/TypeScript/issues/44795
+  // TODO: revisit once TS implements https://github.com/microsoft/TypeScript/issues/32166
+  const notSupportedTSVersionFile = 'NotSupportedTSVersion.d.ts';
   fs.writeFileSync(
-    path.join('./npmDist', packageJSON.types),
+    path.join('./npmDist', notSupportedTSVersionFile),
     // Provoke syntax error to show this message
     `"Package 'graphql' support only TS versions that are ${supportedTSVersions[0]}".`,
   );
+  packageJSON.typesVersions = {
+    ...packageJSON.typesVersions,
+    '*': { '*': [notSupportedTSVersionFile] },
+  };
 
   fs.copyFileSync('./LICENSE', './npmDist/LICENSE');
   fs.copyFileSync('./README.md', './npmDist/README.md');

integrationTests/ts/internalImports-test.ts
@@ -1,0 +1,8 @@
+import type { NameNode } from 'graphql/language';
+
+// Parser class is internal API so so any changes to it are never considered breaking changes.
+// We just want to test that we are able to import it.
+import { Parser } from 'graphql/language/parser';
+
+const parser = new Parser('foo');
+const ast: NameNode = parser.parseName();

integrationTests/ts/test.js
@@ -11,7 +11,9 @@ const tsVersions = Object.keys(dependencies)
 
 for (const version of tsVersions) {
   console.log(`Testing on ${version} ...`);
+  childProcess.execSync(tscPath(version), { stdio: 'inherit' });
+}
 
-  const tscPath = path.join(__dirname, 'node_modules', version, 'bin/tsc');
-  childProcess.execSync(tscPath, { stdio: 'inherit' });
+function tscPath(version) {
+  return path.join(__dirname, 'node_modules', version, 'bin/tsc');
 }