65931cd8db097346be7420cfa000f5728477d0dc
Francisco Marques <franciscopcmarques@gmail.com>
2021-11-02 11:18:45 +0000

    Add exports to MAX_INT and MIN_INT in scalars.ts

--- src/type/scalars.ts
+++ src/type/scalars.ts
@@ -14,8 +14,8 @@ import { GraphQLScalarType } from './definition';
 //
 // n.b. JavaScript's integers are safe between -(2^53 - 1) and 2^53 - 1 because
 // they are internally represented as IEEE 754 doubles.
-const MAX_INT = 2147483647;
-const MIN_INT = -2147483648;
+export const MAX_INT = 2147483647;
+export const MIN_INT = -2147483648;
 
 export const GraphQLInt = new GraphQLScalarType<number>({
   name: 'Int',
