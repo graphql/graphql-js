f70f0d73161b600603919f7dcd0b64a1e03106f3
twof <hello@alex.dev>
2021-11-01 23:56:57 -0700

    add docs to modifiedOutputType

--- src/utilities/applyRequiredStatus.ts
+++ src/utilities/applyRequiredStatus.ts
@@ -6,6 +6,12 @@ import {
   isNonNullType,
 } from '../type/definition';
 
+/**
+ * Implements the "Accounting For Client Controlled Nullability Designators" 
+ * section of the spec. In particular, this function figures out the true return
+ * type of a field by taking into account both the nullability listed in the
+ * schema, and the nullability providing by an operation.
+ */
 export function modifiedOutputType(
   type: GraphQLOutputType,
   required: RequiredStatus,
