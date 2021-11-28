0fb6afc9c31e16b7c5a752d0baaa829e583aa244
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2021-11-28 23:47:08 +0200

    Drop "eslint-plugin-istanbul" and implement as internal ESLint rule (#3388)

--- resources/eslint-internal-rules/index.js
+++ resources/eslint-internal-rules/index.js
@@ -3,11 +3,13 @@
 const onlyASCII = require('./only-ascii.js');
 const noDirImport = require('./no-dir-import.js');
 const requireToStringTag = require('./require-to-string-tag.js');
+const requireCoverageIgnoreReason = require('./require-coverage-ignore-reason.js');
 
 module.exports = {
   rules: {
     'only-ascii': onlyASCII,
     'no-dir-import': noDirImport,
     'require-to-string-tag': requireToStringTag,
+    'require-coverage-ignore-reason': requireCoverageIgnoreReason,
   },
 };
