68701d3337754d13e5e72ece78ca44bbb29f74c4
twof <hello@alex.dev>
2021-12-20 20:18:47 -0800

    prettier

--- src/validation/rules/RequiredStatusOnFieldMatchesDefinitionRule.ts
+++ src/validation/rules/RequiredStatusOnFieldMatchesDefinitionRule.ts
@@ -1,9 +1,19 @@
-import type { FieldNode, NullabilityModifierNode, SupportArrayNode } from '../../language/ast';
+import type {
+  FieldNode,
+  NullabilityModifierNode,
+  SupportArrayNode,
+} from '../../language/ast';
 import { ASTReducer, ASTVisitor, visit } from '../../language/visitor';
 
 import type { ValidationContext } from '../ValidationContext';
 import { modifiedOutputType } from '../../utilities/applyRequiredStatus';
-import { assertListType, getNullableType, GraphQLError, GraphQLOutputType, isListType } from '../..';
+import {
+  assertListType,
+  getNullableType,
+  GraphQLError,
+  GraphQLOutputType,
+  isListType,
+} from '../..';
 
 /**
  * List element nullability designators need to use a depth that is the same as or less than the
@@ -20,19 +30,23 @@ export function RequiredStatusOnFieldMatchesDefinitionRule(
     Field(node: FieldNode) {
       const fieldDef = context.getFieldDef();
       if (fieldDef) {
-        const typeDepth = getTypeDepth(fieldDef.type)
-        const designatorDepth = getDesignatorDepth(node.required)
+        const typeDepth = getTypeDepth(fieldDef.type);
+        const designatorDepth = getDesignatorDepth(node.required);
 
         if (typeDepth > designatorDepth) {
-          context.reportError(new GraphQLError(
-            'List nullability modifier is too shallow.',
-            node.required,
-          ));
+          context.reportError(
+            new GraphQLError(
+              'List nullability modifier is too shallow.',
+              node.required,
+            ),
+          );
         } else if (typeDepth < designatorDepth) {
-          context.reportError(new GraphQLError(
-            'List nullability modifier is too deep.',
-            node.required,
-          ));
+          context.reportError(
+            new GraphQLError(
+              'List nullability modifier is too deep.',
+              node.required,
+            ),
+          );
         }
       }
     },
@@ -44,29 +58,33 @@ export function RequiredStatusOnFieldMatchesDefinitionRule(
     while (isListType(getNullableType(currentType))) {
       const list = assertListType(getNullableType(currentType));
       const elementType = list.ofType as GraphQLOutputType;
-      currentType = elementType
+      currentType = elementType;
       depthCount += 1;
     }
-    return depthCount
+    return depthCount;
   }
 
   function getDesignatorDepth(
-    designator: SupportArrayNode | NullabilityModifierNode | undefined
+    designator: SupportArrayNode | NullabilityModifierNode | undefined,
   ): number {
     const getDepth: ASTReducer<number> = {
       RequiredDesignator: {
-        leave({element}) { return element ?? 0 }
+        leave({ element }) {
+          return element ?? 0;
+        },
       },
 
       OptionalDesignator: {
-        leave({element}) { return element ?? 0 }
+        leave({ element }) {
+          return element ?? 0;
+        },
       },
 
       ListNullabilityDesignator: {
-        leave({element}) {
-          return (element ?? 0) + 1
-        }
-      }
+        leave({ element }) {
+          return (element ?? 0) + 1;
+        },
+      },
     };
 
     return designator !== undefined ? visit(designator, getDepth) : 0;
