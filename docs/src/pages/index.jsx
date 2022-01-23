84968d56e1e8a401d6f0a0a1edc32bc9832fb7ae
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-01-23 20:39:38 +0200

    order imports

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
 
 import styles from './index.module.css';
 
