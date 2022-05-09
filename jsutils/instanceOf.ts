f600b5e8e33a2add9b5ed926ae3b5a7ec88df4f2
GitHub Action Script <please@open.issue>
2022-05-09 12:45:31 +0000

    Deploy  to 'deno' branch

--- jsutils/instanceOf.ts
+++ jsutils/instanceOf.ts
@@ -8,8 +8,7 @@ import { inspect } from './inspect.ts';
 export const instanceOf: (value: unknown, constructor: Constructor) => boolean =
   /* c8 ignore next 6 */
   // FIXME: https://github.com/graphql/graphql-js/issues/2317
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === 'production'
+  globalThis.process?.env.NODE_ENV === 'production'
     ? function instanceOf(value: unknown, constructor: Constructor): boolean {
         return value instanceof constructor;
       }
