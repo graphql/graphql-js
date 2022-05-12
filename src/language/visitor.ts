cd0f76b27286f3b1aeaf32efae3db2e5a20c923a
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-05-12 11:13:09 +0300

    ASTVisitorKeyMap: drop deprecation notice

--- src/language/visitor.ts
+++ src/language/visitor.ts
@@ -79,8 +79,6 @@ type ReducedField<T, R> = T extends null | undefined
 
 /**
  * A KeyMap describes each the traversable properties of each kind of node.
- *
- * @deprecated Please inline it. Will be removed in v17
  */
 export type ASTVisitorKeyMap = {
   [NodeT in ASTNode as NodeT['kind']]?: ReadonlyArray<keyof NodeT>;
