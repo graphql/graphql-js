06cea596d6c575400c9d73806553e114145fb0cc
GitHub Action Script <please@open.issue>
2022-05-19 18:59:51 +0000

    Deploy  to 'deno' branch

--- version.ts
+++ version.ts
@@ -3,13 +3,13 @@
 /**
  * A string containing the version of the GraphQL.js library
  */
-export const version = '16.4.0' as string;
+export const version = '17.0.0-alpha.1' as string;
 /**
  * An object containing the components of the GraphQL.js version string
  */
 export const versionInfo = Object.freeze({
-  major: 16 as number,
-  minor: 4 as number,
+  major: 17 as number,
+  minor: 0 as number,
   patch: 0 as number,
-  preReleaseTag: null as string | null,
+  preReleaseTag: 'alpha.1' as string | null,
 });
