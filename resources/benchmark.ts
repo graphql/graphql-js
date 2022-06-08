f75cc49515a33a29a211fc758d7c021c0cc49322
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-06-08 13:13:18 +0300

    Add comment on why we need `url.pathToFileURL`

--- resources/benchmark.ts
+++ resources/benchmark.ts
@@ -376,10 +376,13 @@ interface BenchmarkSample {
 }
 
 function sampleModule(modulePath: string): BenchmarkSample {
+  // To support Windows we need to use URL instead of path
+  const moduleURL = url.pathToFileURL(modulePath);
+
   const sampleCode = `
     import fs from 'node:fs';
 
-    import { benchmark } from '${url.pathToFileURL(modulePath)}';
+    import { benchmark } from '${moduleURL}';
 
     // warm up, it looks like 7 is a magic number to reliably trigger JIT
     benchmark.measure();
