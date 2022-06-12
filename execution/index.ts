0b89811185edc7775956f38387a346c761c4d736
GitHub Action Script <please@open.issue>
2022-06-12 17:23:53 +0000

    Deploy  to 'deno' branch

--- execution/index.ts
+++ execution/index.ts
@@ -1,16 +1,17 @@
 export { pathToArray as responsePathAsArray } from '../jsutils/Path.ts';
 export {
+  createSourceEventStream,
   execute,
   executeSync,
   defaultFieldResolver,
   defaultTypeResolver,
+  subscribe,
 } from './execute.ts';
 export type {
   ExecutionArgs,
   ExecutionResult,
   FormattedExecutionResult,
 } from './execute.ts';
-export { subscribe, createSourceEventStream } from './subscribe.ts';
 export {
   getArgumentValues,
   getVariableValues,
