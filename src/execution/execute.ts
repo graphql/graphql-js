37b78678652ce157d40e7b0d6ad7377e516f0829
Rob Richard <rob@1stdibs.com>
2022-07-11 16:20:05 -0400

    fix(race): concurrent next calls with defer/stream (#2975)
    * fix(race): concurrent next calls
    
    * refactor test
    
    * use invariant
    
    * disable eslint error
    
    * fix

--- src/execution/execute.ts
+++ src/execution/execute.ts
@@ -1821,7 +1821,16 @@ function yieldSubsequentPayloads(
       },
     );
 
+    if (exeContext.subsequentPayloads.length === 0) {
+      // a different call to next has exhausted all payloads
+      return { value: undefined, done: true };
+    }
     const index = exeContext.subsequentPayloads.indexOf(asyncPayloadRecord);
+    if (index === -1) {
+      // a different call to next has consumed this payload
+      return race();
+    }
+
     exeContext.subsequentPayloads.splice(index, 1);
 
     const incrementalResult: IncrementalResult = {};
