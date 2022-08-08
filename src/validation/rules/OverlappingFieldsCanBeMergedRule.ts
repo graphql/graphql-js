cbd1be58c79c1e1a0517ccd258ae0244df0afec7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-08-08 15:24:27 +0300

    Replace all instances of `ObjMap<boolean>` with ES6 Set
    Motivation: I was inspecting code for unrelated reason and just spotted this pattern.
    I think it worth to refactor it since it easier to understand intention looking on code that uses Set's has/add methods.
    Benchmark shows no measurable changes in CPU/memory usages.

--- src/validation/rules/OverlappingFieldsCanBeMergedRule.ts
+++ src/validation/rules/OverlappingFieldsCanBeMergedRule.ts
@@ -107,7 +107,7 @@ type NodeAndDef = [
 ];
 // Map of array of those.
 type NodeAndDefCollection = ObjMap<Array<NodeAndDef>>;
-type FragmentNames = Array<string>;
+type FragmentNames = ReadonlyArray<string>;
 type FieldsAndFragmentNames = readonly [NodeAndDefCollection, FragmentNames];
 
 /**
@@ -696,7 +696,7 @@ function getFieldsAndFragmentNames(
     return cached;
   }
   const nodeAndDefs: NodeAndDefCollection = Object.create(null);
-  const fragmentNames: ObjMap<boolean> = Object.create(null);
+  const fragmentNames = new Set<string>();
   _collectFieldsAndFragmentNames(
     context,
     parentType,
@@ -704,7 +704,7 @@ function getFieldsAndFragmentNames(
     nodeAndDefs,
     fragmentNames,
   );
-  const result = [nodeAndDefs, Object.keys(fragmentNames)] as const;
+  const result = [nodeAndDefs, [...fragmentNames]] as const;
   cachedFieldsAndFragmentNames.set(selectionSet, result);
   return result;
 }
@@ -736,7 +736,7 @@ function _collectFieldsAndFragmentNames(
   parentType: Maybe<GraphQLNamedType>,
   selectionSet: SelectionSetNode,
   nodeAndDefs: NodeAndDefCollection,
-  fragmentNames: ObjMap<boolean>,
+  fragmentNames: Set<string>,
 ): void {
   for (const selection of selectionSet.selections) {
     switch (selection.kind) {
@@ -756,7 +756,7 @@ function _collectFieldsAndFragmentNames(
         break;
       }
       case Kind.FRAGMENT_SPREAD:
-        fragmentNames[selection.name.value] = true;
+        fragmentNames.add(selection.name.value);
         break;
       case Kind.INLINE_FRAGMENT: {
         const typeCondition = selection.typeCondition;
