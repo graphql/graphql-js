62a08fa9b41e561b3508e51a4b06806686251cf7
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2022-08-09 10:59:17 +0300

    Replace all instances of `ObjMap<boolean>` with ES6 Set (#3688)
    Motivation: I was inspecting code for unrelated reason and just spotted this pattern.
    I think it worth to refactor it since it easier to understand intention looking on code that uses Set's has/add methods.
    Benchmark shows no measurable changes in CPU/memory usages.

--- src/type/validate.ts
+++ src/type/validate.ts
@@ -326,7 +326,7 @@ function validateInterfaces(
   context: SchemaValidationContext,
   type: GraphQLObjectType | GraphQLInterfaceType,
 ): void {
-  const ifaceTypeNames = Object.create(null);
+  const ifaceTypeNames = new Set<string>();
   for (const iface of type.getInterfaces()) {
     if (!isInterfaceType(iface)) {
       context.reportError(
@@ -345,7 +345,7 @@ function validateInterfaces(
       continue;
     }
 
-    if (ifaceTypeNames[iface.name]) {
+    if (ifaceTypeNames.has(iface.name)) {
       context.reportError(
         `Type ${type.name} can only implement ${iface.name} once.`,
         getAllImplementsInterfaceNodes(type, iface),
@@ -353,7 +353,7 @@ function validateInterfaces(
       continue;
     }
 
-    ifaceTypeNames[iface.name] = true;
+    ifaceTypeNames.add(iface.name);
 
     validateTypeImplementsAncestors(context, type, iface);
     validateTypeImplementsInterface(context, type, iface);
@@ -470,16 +470,16 @@ function validateUnionMembers(
     );
   }
 
-  const includedTypeNames = Object.create(null);
+  const includedTypeNames = new Set<string>();
   for (const memberType of memberTypes) {
-    if (includedTypeNames[memberType.name]) {
+    if (includedTypeNames.has(memberType.name)) {
       context.reportError(
         `Union type ${union.name} can only include type ${memberType.name} once.`,
         getUnionMemberTypeNodes(union, memberType.name),
       );
       continue;
     }
-    includedTypeNames[memberType.name] = true;
+    includedTypeNames.add(memberType.name);
     if (!isObjectType(memberType)) {
       context.reportError(
         `Union type ${union.name} can only include Object types, ` +
@@ -551,7 +551,7 @@ function createInputObjectCircularRefsValidator(
   // Modified copy of algorithm from 'src/validation/rules/NoFragmentCycles.js'.
   // Tracks already visited types to maintain O(N) and to ensure that cycles
   // are not redundantly reported.
-  const visitedTypes = Object.create(null);
+  const visitedTypes = new Set<GraphQLInputObjectType>();
 
   // Array of types nodes used to produce meaningful errors
   const fieldPath: Array<GraphQLInputField> = [];
@@ -565,11 +565,11 @@ function createInputObjectCircularRefsValidator(
   // It does not terminate when a cycle was found but continues to explore
   // the graph to find all possible cycles.
   function detectCycleRecursive(inputObj: GraphQLInputObjectType): void {
-    if (visitedTypes[inputObj.name]) {
+    if (visitedTypes.has(inputObj)) {
       return;
     }
 
-    visitedTypes[inputObj.name] = true;
+    visitedTypes.add(inputObj);
     fieldPathIndexByTypeName[inputObj.name] = fieldPath.length;
 
     const fields = Object.values(inputObj.getFields());
