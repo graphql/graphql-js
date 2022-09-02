d2b3f7b7a80f17c68b8d712c2efe64a0a9afcaad
Jovi De Croock <decroockjovi@gmail.com>
2022-09-02 10:00:56 +0200

    run prettier

--- resources/transform-array-destruct.ts
+++ resources/transform-array-destruct.ts
@@ -6,23 +6,27 @@ export function transformArrayDestructuring() {
 
     return (sourceFile: ts.SourceFile) => {
       const visitor = (node: ts.Node): ts.Node => {
-        if(ts.isArrayBindingPattern(node)) {
-          const elements = node.elements.map((el, i) => {
-            if (!el.getText()) {
-              return undefined;
-            }
-            return { key: String(i), name: el.getText() }
-          }).filter(Boolean)
+        if (ts.isArrayBindingPattern(node)) {
+          const elements = node.elements
+            .map((el, i) => {
+              if (!el.getText()) {
+                return undefined;
+              }
+              return { key: String(i), name: el.getText() };
+            })
+            .filter(Boolean);
 
-          const els = elements.map(el => {
-            if (!el) {
-              return undefined;
-            }
-            const key = factory.createIdentifier(el.key)
-            const name = factory.createIdentifier(el.name)
-            return factory.createBindingElement(undefined, key, name)
-          }).filter(Boolean) as Array<ts.BindingElement>
-          return factory.createObjectBindingPattern(els)
+          const els = elements
+            .map((el) => {
+              if (!el) {
+                return undefined;
+              }
+              const key = factory.createIdentifier(el.key);
+              const name = factory.createIdentifier(el.name);
+              return factory.createBindingElement(undefined, key, name);
+            })
+            .filter(Boolean) as Array<ts.BindingElement>;
+          return factory.createObjectBindingPattern(els);
         }
 
         return ts.visitEachChild(node, visitor, context);
@@ -30,5 +34,4 @@ export function transformArrayDestructuring() {
       return ts.visitNode(sourceFile, visitor);
     };
   };
-};
-
+}
