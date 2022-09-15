f9b93106767b1d8cf29693f60f62d0dab1ffaf90
GitHub Action Script <please@open.issue>
2022-09-15 09:25:32 +0000

    Deploy  to 'deno' branch

--- jsutils/promiseForObject.ts
+++ jsutils/promiseForObject.ts
@@ -6,16 +6,15 @@ import type { ObjMap } from './ObjMap.ts';
  * This is akin to bluebird's `Promise.props`, but implemented only using
  * `Promise.all` so it will work with any implementation of ES6 promises.
  */
-export function promiseForObject<T>(
+export async function promiseForObject<T>(
   object: ObjMap<Promise<T>>,
 ): Promise<ObjMap<T>> {
   const keys = Object.keys(object);
   const values = Object.values(object);
-  return Promise.all(values).then((resolvedValues) => {
-    const resolvedObject = Object.create(null);
-    for (let i = 0; i < keys.length; ++i) {
-      resolvedObject[keys[i]] = resolvedValues[i];
-    }
-    return resolvedObject;
-  });
+  const resolvedValues = await Promise.all(values);
+  const resolvedObject = Object.create(null);
+  for (let i = 0; i < keys.length; ++i) {
+    resolvedObject[keys[i]] = resolvedValues[i];
+  }
+  return resolvedObject;
 }
