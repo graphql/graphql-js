54429a745328331ac1dce26b511d81ccbd3403c8
Yaacov Rydzinski <yaacovCR@gmail.com>
2022-12-16 13:20:09 +0200

    add comments to async helpers

--- src/jsutils/after.ts
+++ src/jsutils/after.ts
@@ -1,6 +1,14 @@
 import { isPromise } from './isPromise.js';
 import type { PromiseOrValue } from './PromiseOrValue.js';
 
+/**
+ * Async Helper Function that avoides `.then()`
+ *
+ * It is faster to await a promise prior to returning it from an async function
+ * than to return a promise with `.then()`.
+ *
+ * see: https://github.com/tc39/proposal-faster-promise-adoption
+ */
 export async function after<T, R>(
   promise: Promise<T>,
   onFulfilled: (value: T) => PromiseOrValue<R>,
--- src/jsutils/catchAfter.ts
+++ src/jsutils/catchAfter.ts
@@ -1,3 +1,11 @@
+/**
+ * Async Helper Function that avoides `.then()`
+ *
+ * It is faster to await a promise prior to returning it from an async function
+ * than to return a promise with `.then()`.
+ *
+ * see: https://github.com/tc39/proposal-faster-promise-adoption
+ */
 export async function catchAfter<T, U>(
   promise: Promise<T>,
   onError: (error: any) => U,
