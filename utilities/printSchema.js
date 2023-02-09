b07e77b12be1d42b2258559e835eb73217f3103e
GitHub Action Script <please@open.issue>
2023-02-09 01:54:54 +0000

    Deploy  to 'npm' branch

--- utilities/printSchema.js
+++ utilities/printSchema.js
@@ -48,23 +48,26 @@ function printFilteredSchema(schema, directiveFilter, typeFilter) {
     .join('\n\n');
 }
 function printSchemaDefinition(schema) {
-  if (schema.description == null && isSchemaOfCommonNames(schema)) {
+  const queryType = schema.getQueryType();
+  const mutationType = schema.getMutationType();
+  const subscriptionType = schema.getSubscriptionType();
+  // Special case: When a schema has no root operation types, no valid schema
+  // definition can be printed.
+  if (!queryType && !mutationType && !subscriptionType) {
     return;
   }
-  const operationTypes = [];
-  const queryType = schema.getQueryType();
-  if (queryType) {
-    operationTypes.push(`  query: ${queryType.name}`);
+  // Only print a schema definition if there is a description or if it should
+  // not be omitted because of having default type names.
+  if (schema.description || !hasDefaultRootOperationTypes(schema)) {
+    return (
+      printDescription(schema) +
+      'schema {\n' +
+      (queryType ? `  query: ${queryType.name}\n` : '') +
+      (mutationType ? `  mutation: ${mutationType.name}\n` : '') +
+      (subscriptionType ? `  subscription: ${subscriptionType.name}\n` : '') +
+      '}'
+    );
   }
-  const mutationType = schema.getMutationType();
-  if (mutationType) {
-    operationTypes.push(`  mutation: ${mutationType.name}`);
-  }
-  const subscriptionType = schema.getSubscriptionType();
-  if (subscriptionType) {
-    operationTypes.push(`  subscription: ${subscriptionType.name}`);
-  }
-  return printDescription(schema) + `schema {\n${operationTypes.join('\n')}\n}`;
 }
 /**
  * GraphQL schema define root types for each type of operation. These types are
@@ -79,22 +82,20 @@ function printSchemaDefinition(schema) {
  *   }
  * ```
  *
- * When using this naming convention, the schema description can be omitted.
+ * When using this naming convention, the schema description can be omitted so
+ * long as these names are only used for operation types.
+ *
+ * Note however that if any of these default names are used elsewhere in the
+ * schema but not as a root operation type, the schema definition must still
+ * be printed to avoid ambiguity.
  */
-function isSchemaOfCommonNames(schema) {
-  const queryType = schema.getQueryType();
-  if (queryType && queryType.name !== 'Query') {
-    return false;
-  }
-  const mutationType = schema.getMutationType();
-  if (mutationType && mutationType.name !== 'Mutation') {
-    return false;
-  }
-  const subscriptionType = schema.getSubscriptionType();
-  if (subscriptionType && subscriptionType.name !== 'Subscription') {
-    return false;
-  }
-  return true;
+function hasDefaultRootOperationTypes(schema) {
+  /* eslint-disable eqeqeq */
+  return (
+    schema.getQueryType() == schema.getType('Query') &&
+    schema.getMutationType() == schema.getType('Mutation') &&
+    schema.getSubscriptionType() == schema.getType('Subscription')
+  );
 }
 function printType(type) {
   if ((0, definition_js_1.isScalarType)(type)) {
