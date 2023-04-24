9c6b6da4c2f92891ac859e936ccf776abf2ed4e1
Artem Zakharchenko <kettanaito@gmail.com>
2023-04-24 19:33:16 +0200

    check "globalThis.process" before accessing it

--- src/jsutils/instanceOf.ts
+++ src/jsutils/instanceOf.ts
@@ -10,7 +10,7 @@ export const instanceOf: (value: unknown, constructor: Constructor) => boolean =
   /* c8 ignore next 6 */
   // FIXME: https://github.com/graphql/graphql-js/issues/2317
   // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === 'production'
+  globalThis.process?.env.NODE_ENV === 'production'
     ? function instanceOf(value: unknown, constructor: Constructor): boolean {
         return value instanceof constructor;
       }
