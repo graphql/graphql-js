8c888ebfd63e038590d4ec02f6de7db5f55b2644
GitHub Action Script <please@open.issue>
2023-09-01 11:01:55 +0000

    Deploy  to 'deno' branch

--- type/directives.ts
+++ type/directives.ts
@@ -226,11 +226,12 @@ export const GraphQLSpecifiedByDirective: GraphQLDirective =
     },
   });
 /**
- * Used to declare an Input Object as a OneOf Input Objects.
+ * Used to indicate an Input Object is a OneOf Input Object.
  */
 export const GraphQLOneOfDirective: GraphQLDirective = new GraphQLDirective({
   name: 'oneOf',
-  description: 'Indicates an Input Object is a OneOf Input Object.',
+  description:
+    'Indicates exactly one field must be supplied and this field must not be `null`.',
   locations: [DirectiveLocation.INPUT_OBJECT],
   args: {},
 });
