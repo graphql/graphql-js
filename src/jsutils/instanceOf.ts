61025233e2123b5d3a4b729f5f329706bab19a70
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2023-09-01 15:45:15 +0300

    instanceOf: workaround bundler issue with process.env (#3965)
    Port of #3923 from v16 to main (v17)

--- src/jsutils/instanceOf.ts
+++ src/jsutils/instanceOf.ts
@@ -9,7 +9,7 @@ import { inspect } from './inspect.js';
 export const instanceOf: (value: unknown, constructor: Constructor) => boolean =
   /* c8 ignore next 6 */
   // FIXME: https://github.com/graphql/graphql-js/issues/2317
-  globalThis.process?.env.NODE_ENV === 'production'
+  globalThis.process != null && globalThis.process.env.NODE_ENV === 'production'
     ? function instanceOf(value: unknown, constructor: Constructor): boolean {
         return value instanceof constructor;
       }
