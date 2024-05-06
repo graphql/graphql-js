e47fb5fb748a99cc407781b1d540ca01071154b9
Jeroen Claassens <jeroen.claassens@live.nl>
2024-05-06 14:32:52 +0000

    fix: fixed build-npm script and export mapping

--- resources/utils.ts
+++ resources/utils.ts
@@ -218,6 +218,17 @@ export function writeGeneratedFile(filepath: string, body: string): void {
   fs.writeFileSync(filepath, formatted);
 }
 
+interface PackageJsonExportMapping {
+  import: {
+    types: string;
+    default: string;
+  };
+  require: {
+    types: string;
+    default: string;
+  }
+}
+
 interface PackageJSON {
   description: string;
   version: string;
@@ -225,7 +236,7 @@ interface PackageJSON {
   repository?: { url?: string };
   scripts?: { [name: string]: string };
   type?: string;
-  exports: { [path: string]: string };
+  exports: { [path: string]: string | PackageJsonExportMapping };
   types?: string;
   typesVersions: { [ranges: string]: { [path: string]: Array<string> } };
   devDependencies?: { [name: string]: string };
