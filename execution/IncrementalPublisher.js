8205b0cdc611719ac10967b6182564a31b5c8570
GitHub Action Script <please@open.issue>
2024-06-19 17:37:54 +0000

    Deploy  to 'npm' branch

--- execution/IncrementalPublisher.js
+++ execution/IncrementalPublisher.js
@@ -167,7 +167,6 @@ class IncrementalPublisher {
           id,
           errors: deferredGroupedFieldSetResult.errors,
         });
-        this._incrementalGraph.removeDeferredFragment(deferredFragmentRecord);
       }
       return;
     }
@@ -181,19 +180,13 @@ class IncrementalPublisher {
     }
     for (const deferredFragmentRecord of deferredGroupedFieldSetResult
       .deferredGroupedFieldSetRecord.deferredFragmentRecords) {
-      const id = deferredFragmentRecord.id;
-      // TODO: add test case for this.
-      // Presumably, this can occur if an error causes a fragment to be completed early,
-      // while an asynchronous deferred grouped field set result is enqueued.
-      /* c8 ignore next 3 */
-      if (id === undefined) {
-        continue;
-      }
       const reconcilableResults =
         this._incrementalGraph.completeDeferredFragment(deferredFragmentRecord);
       if (reconcilableResults === undefined) {
         continue;
       }
+      const id = deferredFragmentRecord.id;
+      id !== undefined || (0, invariant_js_1.invariant)(false);
       const incremental = context.incremental;
       for (const reconcilableResult of reconcilableResults) {
         const { bestId, subPath } = this._getBestIdAndSubPath(
