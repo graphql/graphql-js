39f2b78623fd8125d6eb65ecc76bc7d8473a8014
GitHub Action Script <please@open.issue>
2024-08-14 11:50:30 +0000

    Deploy  to 'deno' branch

--- execution/execute.ts
+++ execution/execute.ts
@@ -1697,6 +1697,11 @@ export const defaultTypeResolver: GraphQLTypeResolver<unknown, unknown> =
         if (isPromise(isTypeOfResult)) {
           promisedIsTypeOfResults[i] = isTypeOfResult;
         } else if (isTypeOfResult) {
+          if (promisedIsTypeOfResults.length > 0) {
+            Promise.all(promisedIsTypeOfResults).then(undefined, () => {
+              /* ignore errors */
+            });
+          }
           return type.name;
         }
       }
