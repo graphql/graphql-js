f531737a7acc04e9a84e03e8626ad99e86e3e11e
Jovi De Croock <decroockjovi@gmail.com>
2024-10-18 16:14:20 +0200

    Alternative approach for findSchemaChanges (#4235)
    Alternative to https://github.com/graphql/graphql-js/pull/2181
    
    This adds a new `findSchemaChanges` export and deprecates the breaking
    and dangerous alternative to be removed in v18. While adding
    safe-schema-changes I noticed that we lack directive arg validation
    here, we should probably add these to breaking/dangerous changes in the
    future.

--- src/index.ts
+++ src/index.ts
@@ -477,8 +477,10 @@ export {
   // Compares two GraphQLSchemas and detects breaking changes.
   BreakingChangeType,
   DangerousChangeType,
+  SafeChangeType,
   findBreakingChanges,
   findDangerousChanges,
+  findSchemaChanges,
 } from './utilities/index.js';
 
 export type {
@@ -506,6 +508,7 @@ export type {
   IntrospectionDirective,
   BuildSchemaOptions,
   BreakingChange,
+  SafeChange,
   DangerousChange,
   TypedQueryDocumentNode,
 } from './utilities/index.js';
