84b122c085d76840867a047d010e2ae550270c94
Jovi De Croock <decroockjovi@gmail.com>
2024-10-23 11:33:26 +0200

    AbortController support (#4250)
    This adds support for aborting execution from the outside or resolvers,
    this adds a few tests and tries to make the support as easy as possible.
    
    Do we want to support having abort support on subscriptions, I guess it
    makes sense for server-sent events.
    
    I've chosen 2 places to place these interrupts
    
    - `executeFieldsSerially` - every time we start a new mutation we check
    whether the runtime has interrupted
    - `executeFields` - every time we start executing a new field we check
    whether the runtime has interrupted
    - inside of the catch block as well so we return a singular error, all
    though this doesn't really matter as the consumer would not receive
    anything
      - this here should also take care of deferred fields
    
    When comparing this to `graphql-tools/execute` I am not sure whether we
    want to match this behavior, this throws a DomException which would be a
    whole new exception that gets thrown while normally during execution we
    wrap everything with GraphQLErrors.
    
    Supersedes https://github.com/graphql/graphql-js/pull/3791
    Resolves https://github.com/graphql/graphql-js/issues/3764
    
    Co-authored-by: yaacovCR <yaacovCR@gmail.com>

---
+++ src/execution/__tests__/abort-signal-test.ts
@@ -0,0 +1,352 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import { expectJSON } from '../../__testUtils__/expectJSON.js';
+import { resolveOnNextTick } from '../../__testUtils__/resolveOnNextTick.js';
+
+import type { DocumentNode } from '../../language/ast.js';
+import { parse } from '../../language/parser.js';
+
+import { buildSchema } from '../../utilities/buildASTSchema.js';
+
+import { execute, experimentalExecuteIncrementally } from '../execute.js';
+import type {
+  InitialIncrementalExecutionResult,
+  SubsequentIncrementalExecutionResult,
+} from '../types.js';
+
+async function complete(
+  document: DocumentNode,
+  rootValue: unknown,
+  abortSignal: AbortSignal,
+) {
+  const result = await experimentalExecuteIncrementally({
+    schema,
+    document,
+    rootValue,
+    abortSignal,
+  });
+
+  if ('initialResult' in result) {
+    const results: Array<
+      InitialIncrementalExecutionResult | SubsequentIncrementalExecutionResult
+    > = [result.initialResult];
+    for await (const patch of result.subsequentResults) {
+      results.push(patch);
+    }
+    return results;
+  }
+}
+
+const schema = buildSchema(`
+  type Todo {
+    id: ID
+    text: String
+    author: User
+  }
+
+  type User {
+    id: ID
+    name: String
+  }
+
+  type Query {
+    todo: Todo
+  }
+
+  type Mutation {
+    foo: String
+    bar: String
+  }
+`);
+
+describe('Execute: Cancellation', () => {
+  it('should stop the execution when aborted during object field completion', async () => {
+    const abortController = new AbortController();
+    const document = parse(`
+      query {
+        todo {
+          id
+          author {
+            id
+          }
+        }
+      }
+    `);
+
+    const resultPromise = execute({
+      document,
+      schema,
+      abortSignal: abortController.signal,
+      rootValue: {
+        todo: async () =>
+          Promise.resolve({
+            id: '1',
+            text: 'Hello, World!',
+            /* c8 ignore next */
+            author: () => expect.fail('Should not be called'),
+          }),
+      },
+    });
+
+    abortController.abort('Aborted');
+
+    const result = await resultPromise;
+
+    expectJSON(result).toDeepEqual({
+      data: {
+        todo: null,
+      },
+      errors: [
+        {
+          message: 'Aborted',
+          path: ['todo', 'id'],
+          locations: [{ line: 4, column: 11 }],
+        },
+      ],
+    });
+  });
+
+  it('should stop the execution when aborted during nested object field completion', async () => {
+    const abortController = new AbortController();
+    const document = parse(`
+      query {
+        todo {
+          id
+          author {
+            id
+          }
+        }
+      }
+    `);
+
+    const resultPromise = execute({
+      document,
+      schema,
+      abortSignal: abortController.signal,
+      rootValue: {
+        todo: {
+          id: '1',
+          text: 'Hello, World!',
+          /* c8 ignore next 3 */
+          author: async () =>
+            Promise.resolve(() => expect.fail('Should not be called')),
+        },
+      },
+    });
+
+    abortController.abort('Aborted');
+
+    const result = await resultPromise;
+
+    expectJSON(result).toDeepEqual({
+      data: {
+        todo: {
+          id: '1',
+          author: null,
+        },
+      },
+      errors: [
+        {
+          message: 'Aborted',
+          path: ['todo', 'author', 'id'],
+          locations: [{ line: 6, column: 13 }],
+        },
+      ],
+    });
+  });
+
+  it('should stop deferred execution when aborted', async () => {
+    const abortController = new AbortController();
+    const document = parse(`
+      query {
+        todo {
+          id
+          ... on Todo @defer {
+            text
+            author {
+              id
+            }
+          }
+        }
+      }
+    `);
+
+    const resultPromise = execute({
+      document,
+      schema,
+      rootValue: {
+        todo: async () =>
+          Promise.resolve({
+            id: '1',
+            text: 'hello world',
+            /* c8 ignore next */
+            author: () => expect.fail('Should not be called'),
+          }),
+      },
+      abortSignal: abortController.signal,
+    });
+
+    abortController.abort('Aborted');
+
+    const result = await resultPromise;
+
+    expectJSON(result).toDeepEqual({
+      data: {
+        todo: null,
+      },
+      errors: [
+        {
+          message: 'Aborted',
+          path: ['todo', 'id'],
+          locations: [{ line: 4, column: 11 }],
+        },
+      ],
+    });
+  });
+
+  it('should stop deferred execution when aborted mid-execution', async () => {
+    const abortController = new AbortController();
+    const document = parse(`
+      query {
+        todo {
+          id
+          ... on Todo @defer {
+            text
+            author {
+              ... on Author @defer {
+                id
+              }
+            }
+          }
+        }
+      }
+    `);
+
+    const resultPromise = complete(
+      document,
+      {
+        todo: async () =>
+          Promise.resolve({
+            id: '1',
+            text: 'hello world',
+            /* c8 ignore next 2 */
+            author: async () =>
+              Promise.resolve(() => expect.fail('Should not be called')),
+          }),
+      },
+      abortController.signal,
+    );
+
+    await resolveOnNextTick();
+    await resolveOnNextTick();
+    await resolveOnNextTick();
+
+    abortController.abort('Aborted');
+
+    const result = await resultPromise;
+
+    expectJSON(result).toDeepEqual([
+      {
+        data: {
+          todo: {
+            id: '1',
+          },
+        },
+        pending: [{ id: '0', path: ['todo'] }],
+        hasNext: true,
+      },
+      {
+        completed: [
+          {
+            errors: [
+              {
+                locations: [
+                  {
+                    column: 13,
+                    line: 6,
+                  },
+                ],
+                message: 'Aborted',
+                path: ['todo', 'text'],
+              },
+            ],
+            id: '0',
+          },
+        ],
+        hasNext: false,
+      },
+    ]);
+  });
+
+  it('should stop the execution when aborted mid-mutation', async () => {
+    const abortController = new AbortController();
+    const document = parse(`
+      mutation {
+        foo
+        bar
+      }
+    `);
+
+    const resultPromise = execute({
+      document,
+      schema,
+      abortSignal: abortController.signal,
+      rootValue: {
+        foo: async () => Promise.resolve('baz'),
+        /* c8 ignore next */
+        bar: () => expect.fail('Should not be called'),
+      },
+    });
+
+    abortController.abort('Aborted');
+
+    const result = await resultPromise;
+
+    expectJSON(result).toDeepEqual({
+      data: {
+        foo: 'baz',
+        bar: null,
+      },
+      errors: [
+        {
+          message: 'Aborted',
+          path: ['bar'],
+          locations: [{ line: 4, column: 9 }],
+        },
+      ],
+    });
+  });
+
+  it('should stop the execution when aborted pre-execute', async () => {
+    const abortController = new AbortController();
+    const document = parse(`
+      query {
+        todo {
+          id
+          author {
+            id
+          }
+        }
+      }
+    `);
+    abortController.abort('Aborted');
+    const result = await execute({
+      document,
+      schema,
+      abortSignal: abortController.signal,
+      rootValue: {
+        /* c8 ignore next */
+        todo: () => expect.fail('Should not be called'),
+      },
+    });
+
+    expectJSON(result).toDeepEqual({
+      errors: [
+        {
+          message: 'Aborted',
+        },
+      ],
+    });
+  });
+});
