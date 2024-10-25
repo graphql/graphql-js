7314fd6b3598400700c2ea6060db386b96357cc2
GitHub Action Script <please@open.issue>
2024-10-25 05:19:18 +0000

    Deploy  to 'deno' branch

--- language/ast.ts
+++ language/ast.ts
@@ -279,6 +279,7 @@ export interface DocumentNode {
   readonly kind: Kind.DOCUMENT;
   readonly loc?: Location | undefined;
   readonly definitions: ReadonlyArray<DefinitionNode>;
+  readonly tokenCount?: number | undefined;
 }
 export type DefinitionNode =
   | ExecutableDefinitionNode
--- language/parser.ts
+++ language/parser.ts
@@ -113,7 +113,12 @@ export function parse(
   options?: ParseOptions | undefined,
 ): DocumentNode {
   const parser = new Parser(source, options);
-  return parser.parseDocument();
+  const document = parser.parseDocument();
+  Object.defineProperty(document, 'tokenCount', {
+    enumerable: false,
+    value: parser.tokenCount,
+  });
+  return document;
 }
 /**
  * Given a string containing a GraphQL value (ex. `[42]`), parse the AST for
@@ -187,6 +192,9 @@ export class Parser {
     this._lexer = new Lexer(sourceObj);
     this._options = options;
     this._tokenCounter = 0;
+  }
+  get tokenCount(): number {
+    return this._tokenCounter;
   }
   /**
    * Converts a name lex token into a name parse node.
@@ -1489,9 +1497,9 @@ export class Parser {
   advanceLexer(): void {
     const { maxTokens } = this._options;
     const token = this._lexer.advance();
-    if (maxTokens !== undefined && token.kind !== TokenKind.EOF) {
+    if (token.kind !== TokenKind.EOF) {
       ++this._tokenCounter;
-      if (this._tokenCounter > maxTokens) {
+      if (maxTokens !== undefined && this._tokenCounter > maxTokens) {
         throw syntaxError(
           this._lexer.source,
           token.start,
