634c9f9232062d414bcbbbe24a79b51f77b111d3
GitHub Action Script <please@open.issue>
2024-10-27 15:44:52 +0000

    Deploy  to 'deno' branch

--- execution/execute.ts
+++ execution/execute.ts
@@ -676,14 +676,6 @@ function executeFields(
   try {
     for (const [responseName, fieldDetailsList] of groupedFieldSet) {
       const fieldPath = addPath(path, responseName, parentType.name);
-      const abortSignal = exeContext.validatedExecutionArgs.abortSignal;
-      if (abortSignal?.aborted) {
-        throw locatedError(
-          new Error(abortSignal.reason),
-          toNodes(fieldDetailsList),
-          pathToArray(fieldPath),
-        );
-      }
       const result = executeField(
         exeContext,
         parentType,
@@ -1616,13 +1608,22 @@ function completeObjectValue(
   incrementalContext: IncrementalContext | undefined,
   deferMap: ReadonlyMap<DeferUsage, DeferredFragmentRecord> | undefined,
 ): PromiseOrValue<GraphQLWrappedResult<ObjMap<unknown>>> {
+  const validatedExecutionArgs = exeContext.validatedExecutionArgs;
+  const abortSignal = validatedExecutionArgs.abortSignal;
+  if (abortSignal?.aborted) {
+    throw locatedError(
+      new Error(abortSignal.reason),
+      toNodes(fieldDetailsList),
+      pathToArray(path),
+    );
+  }
   // If there is an isTypeOf predicate function, call it with the
   // current result. If isTypeOf returns false, then raise an error rather
   // than continuing execution.
   if (returnType.isTypeOf) {
     const isTypeOf = returnType.isTypeOf(
       result,
-      exeContext.validatedExecutionArgs.contextValue,
+      validatedExecutionArgs.contextValue,
       info,
     );
     if (isPromise(isTypeOf)) {
