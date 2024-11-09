05b06d4f07aec1bb27ddc6fc790dc06f78a2829c
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-09 08:53:29 -0500

    allow type level namespace access too

--- src/language/kinds.ts
+++ src/language/kinds.ts
@@ -1,8 +1,1 @@
-import * as kinds_ from './kinds_.js'; // eslint-disable-line
-
 export * as Kind from './kinds_.js';
-
-/**
- * The set of allowed kind values for AST nodes.
- */
-export type Kind = (typeof kinds_)[keyof typeof kinds_];
