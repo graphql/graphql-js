cece3f72d71f05e24ff6ef97b2935f4428bbdac1
Jason Kuhrt <jasonkuhrt@me.com>
2024-11-09 09:05:17 -0500

    bring back type

--- src/language/kinds.ts
+++ src/language/kinds.ts
@@ -1,1 +1,6 @@
+/* eslint-disable import/no-namespace */
+import type * as Kind_ from './kinds_.js';
+
 export * as Kind from './kinds_.js';
+
+export type Kind = (typeof Kind_)[keyof typeof Kind_];
