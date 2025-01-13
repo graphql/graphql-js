0a3585baea9008253548def2d02b61dbbcc140bc
enisdenjo <denis@denelop.com>
2025-01-13 16:27:42 +0100

    move formatError to graphqlerror and re-export
    Follow-up #4324

--- src/error/GraphQLError.js
+++ src/error/GraphQLError.js
@@ -1,3 +1,4 @@
+import devAssert from '../jsutils/devAssert';
 import isObjectLike from '../jsutils/isObjectLike';
 import { SYMBOL_TO_STRING_TAG } from '../polyfills/symbols';
 
@@ -6,9 +7,6 @@ import type { Source } from '../language/source';
 import type { SourceLocation } from '../language/location';
 import { getLocation } from '../language/location';
 import { printLocation, printSourceLocation } from '../language/printLocation';
-
-import { formatError } from './formatError';
-import type { GraphQLFormattedError } from './formatError';
 
 /**
  * A GraphQLError describes an Error found during the parse, validate, or
@@ -198,3 +196,48 @@ export function printError(error: GraphQLError): string {
 
   return output;
 }
+
+/**
+ * Given a GraphQLError, format it according to the rules described by the
+ * Response Format, Errors section of the GraphQL Specification.
+ */
+export function formatError(error: GraphQLError): GraphQLFormattedError {
+  devAssert(error, 'Received null or undefined error.');
+  const message = error.message ?? 'An unknown error occurred.';
+  const locations = error.locations;
+  const path = error.path;
+  const extensions = error.extensions;
+
+  return extensions && Object.keys(extensions).length > 0
+    ? { message, locations, path, extensions }
+    : { message, locations, path };
+}
+
+/**
+ * @see https://github.com/graphql/graphql-spec/blob/master/spec/Section%207%20--%20Response.md#errors
+ */
+export type GraphQLFormattedError = {|
+  /**
+   * A short, human-readable summary of the problem that **SHOULD NOT** change
+   * from occurrence to occurrence of the problem, except for purposes of
+   * localization.
+   */
+  +message: string,
+  /**
+   * If an error can be associated to a particular point in the requested
+   * GraphQL document, it should contain a list of locations.
+   */
+  +locations: $ReadOnlyArray<SourceLocation> | void,
+  /**
+   * If an error can be associated to a particular field in the GraphQL result,
+   * it _must_ contain an entry with the key `path` that details the path of
+   * the response field which experienced the error. This allows clients to
+   * identify whether a null result is intentional or caused by a runtime error.
+   */
+  +path: $ReadOnlyArray<string | number> | void,
+  /**
+   * Reserved for implementors to extend the protocol however they see fit,
+   * and hence there are no additional restrictions on its contents.
+   */
+  +extensions?: { [key: string]: mixed, ... },
+|};
