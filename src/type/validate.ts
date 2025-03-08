85066a09b1087ef67e1c5c72714010192396f45a
Benjie <benjie@jemjie.com>
2025-03-08 07:36:36 +0000

    Reject deprecated fields when interface field is not deprecated (#3986)
    This PR implements GraphQL RFC
    https://github.com/graphql/graphql-spec/pull/1053; please see that PR
    for details.
    
    ---------
    
    Co-authored-by: Jovi De Croock <decroockjovi@gmail.com>

--- src/type/validate.ts
+++ src/type/validate.ts
@@ -573,6 +573,21 @@ function validateTypeImplementsInterface(
         }
       }
     }
+
+    // Asserts that field is not deprecated unless interface field is
+    if (
+      typeField.deprecationReason != null &&
+      ifaceField.deprecationReason == null
+    ) {
+      context.reportError(
+        `Interface field ${iface.name}.${ifaceField.name} is not deprecated, so ` +
+          `implementation field ${type.name}.${typeField.name} must not be deprecated.`,
+        [
+          getDeprecatedDirectiveNode(typeField.astNode),
+          typeField.astNode?.type,
+        ],
+      );
+    }
   }
 }
 
