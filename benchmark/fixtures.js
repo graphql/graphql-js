beb9fa77d46dbd9ca4b1c61687320bac2a290c2f
jdecroock <decroockjovi@gmail.com>
2025-05-04 10:25:18 +0200

    Increase print/visit performance (#4312)
    This replaces our expensive method that changes the underlying V8 shape
    multiple times with a loop that preserves the identity as much as
    possible.
    
    ```
    ⏱   Print kitchen sink document
      1 tests completed.
      2 tests completed.
    
      HEAD x 9,290 ops/sec ±0.21% x 1.51 KB/op (24 runs sampled)
      BASE x 2,645 ops/sec ±0.18% x 2.18 KB/op (11 runs sampled)
    ```
    
    ---------
    
    Co-authored-by: Benjie <code@benjiegillam.com>

--- benchmark/fixtures.js
+++ benchmark/fixtures.js
@@ -8,3 +8,8 @@ export const bigSchemaSDL = fs.readFileSync(
 export const bigSchemaIntrospectionResult = JSON.parse(
   fs.readFileSync(new URL('github-schema.json', import.meta.url), 'utf8'),
 );
+
+export const bigDocumentSDL = fs.readFileSync(
+  new URL('kitchen-sink.graphql', import.meta.url),
+  'utf8',
+);
