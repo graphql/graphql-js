3355470d461659df974fd00b5cf5fec3b3e04b9d
sarahxsanders <sasanders26@gmail.com>
2025-05-20 21:00:43 -0400

    feedback

--- website/pages/docs/_meta.ts
+++ website/pages/docs/_meta.ts
@@ -23,7 +23,7 @@ const meta = {
   'cursor-based-pagination': '',
   'custom-scalars': '',
   'advanced-custom-scalars': '',
-  'query-complexity-controls': '',
+  'operation-complexity-controls': '',
   'n1-dataloader': '',
   'resolver-anatomy': '',
   'graphql-errors': '',
