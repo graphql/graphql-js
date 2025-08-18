a4570ba62fc58c42b78bcf2c22413a6f5bb781a9
Yaacov Rydzinski <yaacovCR@gmail.com>
2025-08-18 04:36:59 +0300

    f

--- integrationTests/dev-jest/index.test.js
+++ integrationTests/dev-jest/index.test.js
@@ -9,7 +9,7 @@ class FakeGraphQLObjectType {
   }
 }
 
-describe('Jest with SWC development mode tests', () => {
+describe('Jest development mode tests', () => {
   test('isObjectType should throw in development mode for instances from another realm/module', () => {
     expect(() => isObjectType(new FakeGraphQLObjectType())).toThrowError(
       /from another module or realm/,
