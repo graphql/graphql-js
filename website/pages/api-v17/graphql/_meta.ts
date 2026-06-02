3bed514b5f336a7fa7954f57542442caa4139857
Yaacov Rydzinski <yaacovCR@gmail.com>
2026-06-02 16:04:24 +0300

    docs: update documentation for v17 release candidate

--- website/pages/api-v17/graphql/_meta.ts
+++ website/pages/api-v17/graphql/_meta.ts
@@ -3,6 +3,10 @@ const meta = {
     title: 'Category: Development Mode',
     href: '/api-v17/graphql#category-development-mode',
   },
+  diagnostics: {
+    title: 'Category: Diagnostics',
+    href: '/api-v17/graphql#category-diagnostics',
+  },
   'request-pipeline': {
     title: 'Category: Request Pipeline',
     href: '/api-v17/graphql#category-request-pipeline',
